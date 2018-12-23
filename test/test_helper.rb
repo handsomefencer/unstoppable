460253182e5115c5cd681c4237e24c817d5e0091
schadenfred <fred.schoeneman@gmail.com>
2018-12-23 01:35:50 -0800

    removed unused test gems and require statements

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,7 @@ require "roro"
 require "minitest/autorun"
 require "minitest/spec"
 require "minitest/given"
-require "minitest/stub_any_instance"
-require "mocha"
-require "rr"
+
 
 require "thor_helper"
 include TestHelper::Files::Assertions
