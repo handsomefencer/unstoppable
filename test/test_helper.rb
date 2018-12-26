fbfc053a35d0f3d751ed3d48e94e6fd8bbcea70c
schadenfred <fred.schoeneman@gmail.com>
2018-12-26 00:40:45 -0800

    well underway

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,9 @@ require "roro"
 require "minitest/autorun"
 require "minitest/spec"
 require "minitest/given"
-
-
 require "thor_helper"
+
+require "generators/shared_expectations"
+
+include Roro::Test::SharedExpectations
 include TestHelper::Files::Assertions
