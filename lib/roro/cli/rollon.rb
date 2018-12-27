efb11ed6095baebfe6843d3b63ac59f002007b5f
schadenfred <fred.schoeneman@gmail.com>
2018-12-26 20:39:04 -0800

    blah

--- lib/roro/cli/rollon.rb
+++ lib/roro/cli/rollon.rb
@@ -2,17 +2,9 @@
 #
 #   class CLI < Thor
 #
-#     desc "dockerize", "Generates files necessary to dockerize your existing Rails project, along with a set of files for continuous deployment using CircleCI and deployment ussing sshkit."
-#
-#     def dockerize
-#       configurate
-#       copy_base_files
-#       append_to_existing_files
-#     end
-#
+#     method_option :env_vars, type: :hash, default: {}, desc: "Pass a list of environment variables like so: env:var", banner: "key1:value1 key2:value2"
 #     desc "greenfield", "Generates files necessary to greenfield a new app within a dockerized rails container, along with a set of files necessary for continuous deployment using CircleCI"
 #     method_option :interactive, desc: "Set up your environment variables as you go."
-#     method_option :env_vars, type: :hash, default: {}, desc: "Pass a list of environment variables like so: env:var", banner: "key1:value1 key2:value2"
 #
 #     def greenfield(app=nil)
 #       configurate
@@ -21,9 +13,9 @@
 #         raise Roro::Error.new("Oops -- Roro can't greenfield unless the current directory is empty.")
 #       end
 #       copy_base_files
-#       copy_file "gitignore", ".gitignore"
-#       copy_file "Gemfile"
-#       copy_file "Gemfile.lock"
+#       copy_file "new_rails_app/gitignore", ".gitignore"
+#       copy_file "new_rails_app/Gemfile"
+#       copy_file "new_rails_app/Gemfile.lock"
 #       append_to_existing_files
 #     end
 #   end
