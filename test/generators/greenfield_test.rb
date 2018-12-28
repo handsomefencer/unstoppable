62eae014c1e0e2a6c2370d97bc810964edb1ca74
schadenfred <fred.schoeneman@gmail.com>
2018-12-27 19:05:11 -0800

    try force

--- test/generators/greenfield_test.rb
+++ test/generators/greenfield_test.rb
@@ -6,7 +6,6 @@ describe Roro::CLI do
 
   Given(:prepare) {
     prepare_destination
-    # FileUtils.copy_entry "../test/dummy", "dummy"
     Dir.chdir 'greenfield'
   }
 
@@ -17,24 +16,20 @@ describe Roro::CLI do
     And { Dir.empty?(Dir.pwd).must_equal true}
   end
 
-  # Given {
-  #   Dir.chdir('../') if Dir.pwd.match /greenfield/
-  #   prepare_destination
-  #
-  #   Dir.chdir 'tmp/greenfield'
-  # }
-  #
   describe "usage" do
 
     Given { prepare }
     Given { subject.greenfield }
-  #
-    # generated_files = %w( )
+
     describe "must generate files" do
 
       Then do
 
-        generated_files = %w( Gemfile docker-compose.yml Dockerfile Gemfile.lock)
+        generated_files = [
+          "Gemfile",
+          "docker-compose.yml",
+          "Dockerfile",
+          "Gemfile.lock"]
         generated_files.each do |generated_file|
 
           assert_file generated_file
