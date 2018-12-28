df541b2492d0a7af6b0575c567c395f28009e906
schadenfred <fred.schoeneman@gmail.com>
2018-12-28 15:44:00 -0800

    run within bin/

--- test/generators/rollon_test.rb
+++ test/generators/rollon_test.rb
@@ -8,115 +8,68 @@ describe Roro::CLI do
   Given { FileUtils.copy_entry "../test/dummy/", "dummy" }
   Given { Dir.chdir 'dummy' }
 
+  Given(:env_vars) { %w(
+    APP_NAME=sooperdooper
+    DEPLOY_TAG=\${CIRCLE_SHA1:0:7}
+    DOCKERHUB_ORG=your-docker-hub-org-name
+    DOCKERHUB_PASS=your-docker-hub-password
+    DOCKERHUB_USER=your-docker-hub-user-name
+    SERVER_HOST=ip-address-of-your-server
+    SERVER_PORT=22
+    SERVER_USER=root ) }
+
+  Given { subject.rollon }
+
   describe "must create" do
 
-    Given { subject.rollon }
+    Then {
 
-    describe "./.circleci" do
+      assert_file 'Gemfile', /gem \'pg\'/
+      assert_file 'Gemfile', /gem \'sshkit\'/
+      assert_file '.gitignore', /docker\/\*\*\/\*.key/
+      assert_file '.gitignore', /docker\/\*\*\/\*.env/
 
-      Then {
-        assert_file '.circleci'
-        assert_file '.circleci/config.yml'
-        assert_file '.circleci/config.yml.workflow-example'
-        assert_file '.circleci/Rakefile' }
-    end
+      assert_file '.circleci'
+      assert_file '.circleci/config.yml'
+      assert_file '.circleci/config.yml.workflow-example'
+      assert_file '.circleci/Rakefile'
 
-    describe "application root files" do
+      assert_file 'config'
+      assert_file 'config/database.yml', /ENV.fetch\('POSTGRES_DB/
 
-      Then {
-        assert_file 'Gemfile', /gem \'pg\'/
-        assert_file '.gitignore', /docker\/\*\*\/\*.key/
-        assert_file '.gitignore', /docker\/\*\*\/\*.env/
-        assert_file 'Gemfile', /gem \'sshkit\'/
+      assert_file 'docker'
 
-      }
-    end
+      assert_file 'docker/containers'
 
-    describe "./docker" do
+      assert_file 'docker/containers/app'
+      assert_file 'docker/containers/app/development.env',
+        /RAILS_ENV=development/
+      assert_file 'docker/containers/app/production.env',
+        /RAILS_ENV=production/
+      assert_file 'docker/containers/app/Dockerfile',
+        /your-docker-hub-email/
 
-      Then { assert_file 'docker' }
+      assert_file 'docker/containers/database'
+      assert_file 'docker/containers/database/development.env',
+        /POSTGRES_PASSWORD=your-postgres-password/
+      assert_file 'docker/containers/database/production.env',
+        /POSTGRES_PASSWORD=your-postgres-password/
 
-      describe "/containers" do
+      assert_file 'docker/containers/web'
+      assert_file 'docker/containers/web/app.conf'
+      assert_file 'docker/containers/web/production.env',
+        /CA_SSL=true/
+      assert_file 'docker/containers/web/Dockerfile',
+        /tmp\/sooperdooper.nginx/
 
-        Then { assert_file 'docker/containers' }
+      assert_file 'docker/env_files'
+      env_vars.each { |env_var|
+        assert_file 'docker/env_files/circleci.env', /export #{env_var}/ }
 
-        describe "/app" do
+      assert_file 'docker/keys'
 
-          Then { assert_file 'docker/containers/app' }
-
-          And {
-            assert_file 'docker/containers/app/development.env',
-              /RAILS_ENV=development/
-            assert_file 'docker/containers/app/production.env',
-              /RAILS_ENV=production/
-            assert_file 'docker/containers/app/Dockerfile',
-              /your-docker-hub-email/ }
-        end
-
-        describe "/database" do
-
-          Then { assert_file 'docker/containers/database' }
-
-          And {
-            assert_file 'docker/containers/database/development.env',
-              /POSTGRES_PASSWORD=your-postgres-password/
-            assert_file 'docker/containers/database/production.env',
-              /POSTGRES_PASSWORD=your-postgres-password/ }
-        end
-
-        describe "/web" do
-
-          Then {
-            assert_file 'docker/containers/web'
-            assert_file 'docker/containers/web/app.conf'
-          }
-
-          And {
-            assert_file 'docker/containers/web/production.env',
-              /CA_SSL=true/
-            assert_file 'docker/containers/web/Dockerfile',
-              /tmp\/sooperdooper.nginx/ }
-        end
-      end
-
-      describe "/env_files" do
-
-        Given(:env_vars) { %w(
-          APP_NAME=sooperdooper
-          DEPLOY_TAG=\${CIRCLE_SHA1:0:7}
-          DOCKERHUB_ORG=your-docker-hub-org-name
-          DOCKERHUB_PASS=your-docker-hub-password
-          DOCKERHUB_USER=your-docker-hub-user-name
-          SERVER_HOST=ip-address-of-your-server
-          SERVER_PORT=22
-          SERVER_USER=root ) }
-
-        Then { assert_file 'docker/env_files' }
-
-        And {
-          env_vars.each { |env_var| assert_file 'docker/env_files/circleci.env', /export #{env_var}/ } }
-      end
-
-      describe "/keys" do
-
-        Then { assert_file 'docker/keys' }
-      end
-
-      describe "/overrides" do
-
-        Then { assert_file 'docker/overrides' }
-
-        And {
-          assert_file 'docker/overrides/circleci.yml'
-          assert_file 'docker/overrides/production.yml' }
-      end
-    end
-
-    describe "./config" do
-
-      Then { assert_file 'config' }
-
-      And { assert_file 'config/database.yml', /ENV.fetch\('POSTGRES_DB/ }
-    end
+      assert_file 'docker/overrides'
+      assert_file 'docker/overrides/circleci.yml'
+      assert_file 'docker/overrides/production.yml' }
   end
 end
