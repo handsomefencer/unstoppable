30a7ef0313b44ace2a457fade44573bb5757e80c
schadenfred <fred.schoeneman@gmail.com>
2018-12-29 14:27:10 -0800

    removed recursive

--- lib/roro/cli/templates/greenfield/greenfield-entrypoint.sh
+++ lib/roro/cli/templates/greenfield/greenfield-entrypoint.sh
@@ -3,4 +3,3 @@
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
 exec mv -f config/database.yml.example config/database.yml
 exec bundle exec rails s -p 3000 -b '0.0.0.0'
-exec greenfield-entrypoint.sh
