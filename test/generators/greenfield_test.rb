ff4f0800feb766a986b0dd1da3d16613da316a4c
schadenfred <fred.schoeneman@gmail.com>
2019-01-01 04:29:25 -0800

    major test refactor

--- test/generators/greenfield_test.rb
+++ test/generators/greenfield_test.rb
@@ -1,36 +1,32 @@
-require "test_helper"
-
-describe Roro::CLI do
-
-  Given(:subject) { Roro::CLI.new }
-
-  Given(:prepare) do
-    prepare_destination
-    
-    Dir.chdir 'greenfield'
-  end
-
-  describe "usage" do
-
-    Given { prepare }
-    Given { subject.copy_greenfield_files }
-
-    describe "must generate files" do
-
-      Then do
-
-        generated_files = [
-          "Gemfile",
-          "config/database.yml.example",
-          "Dockerfile",
-          "docker-compose.yml",
-          "Gemfile.lock"
-        ]
-        generated_files.each do |generated_file|
-
-          assert_file generated_file
-        end
-      end
-    end
-  end
-end
+# require "test_helper"
+#
+# describe Roro::CLI do
+#
+#   Given(:subject) { Roro::CLI.new }
+#
+#   Given { prepare_destination }
+#   Given { Dir.chdir 'greenfield' }
+#
+#   describe "usage" do
+#
+#     Given { subject.copy_greenfield_files }
+#
+#     describe "must generate files" do
+#
+#       Then do
+#
+#         generated_files = [
+#           "Gemfile",
+#           "config/database.yml.example",
+#           "Dockerfile",
+#           "docker-compose.yml",
+#           "Gemfile.lock"
+#         ]
+#         generated_files.each do |generated_file|
+#
+#           assert_file generated_file
+#         end
+#       end
+#     end
+#   end
+# end
