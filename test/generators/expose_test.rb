9afdaedeb660676168d5120f7ddd9c761a55c3eb
schadenfred <fred.schoeneman@gmail.com>
2019-01-01 04:41:06 -0800

    finished refactor

--- test/generators/expose_test.rb
+++ test/generators/expose_test.rb
@@ -1,48 +1,48 @@
-# require 'test_helper'
-#
-# describe "Roro::CLI" do
-#
-#   Given(:subject) { Roro::CLI.new }
-#   Given { prepare_destination( 'dummy_roro') }
-#
-#   Given { subject.generate_keys }
-#   Given { subject.obfuscate }
-#
-#   Given(:env_files) { HandsomeFencer::Crypto.source_files('.', '.env') }
-#
-#   describe ":expose(environment)" do
-#
-#     Given { env_files.each { |file| FileUtils.rm file } }
-#     Given { env_files.each { |file| refute_file file } }
-#
-#     describe "with key_file" do
-#
-#       Given { subject.expose('development') }
-#
-#       Then { assert_file 'docker/containers/app/development.env' }
-#
-#       And { refute_file 'docker/containers/app/production.env' }
-#     end
-#
-#     describe "with ENV_KEY" do
-#
-#       Given(:passkey) { File.read 'docker/keys/development.key' }
-#       Given { ENV['DEVELOPMENT_KEY'] = passkey }
-#       Given { FileUtils.rm 'docker/keys/development.key' }
-#       Given { subject.expose('development') }
-#
-#       Then { assert_file 'docker/containers/app/development.env' }
-#
-#       And { refute_file 'docker/containers/app/production.env' }
-#     end
-#   end
-#
-#   describe ":expose" do
-#
-#     Given { env_files.each { |file| FileUtils.rm file } }
-#     Given { env_files.each { |file| refute_file file } }
-#     Given { subject.expose }
-#
-#     Then { env_files.each { |file| assert_file file } }
-#   end
-# end
+require 'test_helper'
+
+describe "Roro::CLI" do
+
+  Given(:subject) { Roro::CLI.new }
+  Given { prepare_destination 'dummy_roro' }
+
+  Given { subject.generate_keys }
+  Given { subject.obfuscate }
+
+  Given(:env_files) { HandsomeFencer::Crypto.source_files('.', '.env') }
+
+  describe ":expose(environment)" do
+
+    Given { env_files.each { |file| FileUtils.rm file } }
+    Given { env_files.each { |file| refute_file file } }
+
+    describe "with key_file" do
+
+      Given { subject.expose('development') }
+
+      Then { assert_file 'docker/containers/app/development.env' }
+
+      And { refute_file 'docker/containers/app/production.env' }
+    end
+
+    describe "with ENV_KEY" do
+
+      Given(:passkey) { File.read 'docker/keys/development.key' }
+      Given { ENV['DEVELOPMENT_KEY'] = passkey }
+      Given { FileUtils.rm 'docker/keys/development.key' }
+      Given { subject.expose('development') }
+
+      Then { assert_file 'docker/containers/app/development.env' }
+
+      And { refute_file 'docker/containers/app/production.env' }
+    end
+  end
+
+  describe ":expose" do
+
+    Given { env_files.each { |file| FileUtils.rm file } }
+    Given { env_files.each { |file| refute_file file } }
+    Given { subject.expose }
+
+    Then { env_files.each { |file| assert_file file } }
+  end
+end
--- test/generators/obfuscate_test.rb
+++ test/generators/obfuscate_test.rb
@@ -1,35 +1,34 @@
-# require 'test_helper'
-#
-# describe "Roro::CLI" do
-#
-#   Given(:subject) { Roro::CLI.new }
-#   Given { prepare_destination }
-#   Given { Dir.chdir 'dummy_roro' }
-#
-#   Given { subject.generate_keys }
-#
-#   describe ":obfuscate(environment)" do
-#
-#     Given { refute_file 'docker/containers/app/development.env.enc' }
-#     Given { assert_file 'docker/containers/app/production.env' }
-#     Given { subject.obfuscate('development') }
-#
-#     Then {
-#       assert_file 'docker/containers/app/development.env.enc'
-#       assert_file 'docker/containers/web/development.env.enc' }
-#
-#     And {
-#       refute_file 'docker/containers/app/production.env.enc'
-#     }
-#   end
-#
-#   describe ":obfuscate" do
-#
-#     Given { subject.obfuscate }
-#
-#     Then {
-#       assert_file 'docker/containers/app/development.env.enc'
-#       assert_file 'docker/containers/web/development.env.enc'
-#       assert_file 'docker/containers/web/production.env.enc' }
-#   end
-# end
+require 'test_helper'
+
+describe "Roro::CLI" do
+
+  Given(:subject) { Roro::CLI.new }
+  Given { prepare_destination 'dummy_roro' }
+
+  Given { subject.generate_keys }
+
+  describe ":obfuscate(environment)" do
+
+    Given { refute_file 'docker/containers/app/development.env.enc' }
+    Given { assert_file 'docker/containers/app/production.env' }
+    Given { subject.obfuscate('development') }
+
+    Then {
+      assert_file 'docker/containers/app/development.env.enc'
+      assert_file 'docker/containers/web/development.env.enc' }
+
+    And {
+      refute_file 'docker/containers/app/production.env.enc'
+    }
+  end
+
+  describe ":obfuscate" do
+
+    Given { subject.obfuscate }
+
+    Then {
+      assert_file 'docker/containers/app/development.env.enc'
+      assert_file 'docker/containers/web/development.env.enc'
+      assert_file 'docker/containers/web/production.env.enc' }
+  end
+end
