a2d0c54d85191ff35716dfdd36180abebb97774b
fred schoeneman <fred.schoeneman@gmail.com>
2019-01-02 13:47:22 -0800

    blah

--- test/generators/rubygem_test.rb
+++ test/generators/rubygem_test.rb
@@ -4,23 +4,29 @@ describe Roro::CLI do
 
   Given(:subject) { Roro::CLI.new }
 
-  Given { prepare_destination 'greenfield' }
+  Given { prepare_destination 'dummy_gem' }
 
   describe "usage" do
 
     Given { subject.ruby_gem }
+    Then { subject.env_vars.must_equal "blah" }
 
     describe "must generate files" do
 
       Then do
 
-        generated_files = [
-          '.circleci/config.yml'
-        ]
-        generated_files.each do |generated_file|
+        assert_file '.docker-compose.yml'
 
-          assert_file generated_file
-        end
+        assert_file '.circleci'
+        assert_file '.circleci/config.yml'
+        assert_file '.circleci/setup-gem-credentials.sh'
+# byebug
+        assert_file 'docker'
+
+        assert_file 'docker/containers'
+
+        assert_file 'docker/containers/app'
+        assert_file 'docker/containers/app/Dockerfile'
       end
     end
   end
