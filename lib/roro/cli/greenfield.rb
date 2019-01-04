d1e76c3a475df36f86820ed3e15f9bd3e1c0ce38
fred schoeneman <fred.schoeneman@gmail.com>
2019-01-03 16:27:43 -0800

    one extra chown

--- lib/roro/cli/greenfield.rb
+++ lib/roro/cli/greenfield.rb
@@ -17,6 +17,7 @@ module Roro
         raise Roro::Error.new("Oops -- Roro can't greenfield a new Rails app for you unless the current directory is empty.")
       end
       copy_greenfield_files
+      system 'sudo chown -R $USER .' if OS.linux?
       system 'sudo docker-compose run web rails new . --force --database=postgresql --skip-bundle'
       system 'sudo chown -R $USER .' if OS.linux?
       system 'sudo docker-compose build'
