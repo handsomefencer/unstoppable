35b44b67ce21bf366a3c3b2e6ca16b260fdd1b47
schadenfred <fred.schoeneman@gmail.com>
2020-08-05 01:06:34 -0700

    good stopping poin, most templatable files have been templated.

--- lib/roro/cli/base.rb
+++ lib/roro/cli/base.rb
@@ -7,9 +7,9 @@ module Roro
 
       def get_configuration_variables
         options["interactive"] ? set_interactively : set_from_defaults
-        @env_hash['DEPLOY_TAG'] = "${CIRCLE_SHA1:0:7}"
-        @env_hash['SERVER_PORT'] = "22"
-        @env_hash['SERVER_USER'] = "root"
+        @env_hash[:deploy_tag] = "${CIRCLE_SHA1:0:7}"
+        @env_hash[:server_port] = "22"
+        @env_hash[:server_user] = "root"
         @env_hash
       end
 
@@ -20,12 +20,13 @@ module Roro
         end
         @env_hash
       end
-
+      
       def choices 
         { default: 'y', limited_to: ["y", "n"] }
       end
-
+      
       def set_interactively
+        @env_hash = configuration_hash
         @env_hash.map do |key, prompt|
           answer = ask("Please provide #{prompt.keys.first} or hit enter to accept: \[ #{prompt.values.first} \]")
           @env_hash[key] = (answer == "") ? prompt.values.first : answer
@@ -37,7 +38,7 @@ module Roro
       end  
       
       def copy_docker_compose 
-        copy_file "base/docker-compose.yml"
+        template "base/docker-compose.yml", 'docker-compose.yml', @env_hash
       end
       
       def config_std_out_true
@@ -53,7 +54,7 @@ module Roro
       end
       
       def copy_roro 
-        directory 'base/roro', 'roro', force: true
+        directory 'base/roro', 'roro', @env_hash
       end
   
       def insert_roro_gem_into_gemfile
@@ -80,23 +81,29 @@ module Roro
 
     def configuration_hash
       {
-        "APP_NAME" => {
+        app_name: {
           "the name of your app" => `pwd`.split('/').last.strip! },
-        "RUBY_VERSION" => {
+        ruby_version: {
           "the version of ruby you'd like" => `ruby -v`.scan(/\d.\d/).first },
-        "SERVER_HOST" => {
+        server_host: {
           "the ip address of your server" => "ip-address-of-your-server"},
-        "DOCKERHUB_EMAIL" => {
+        database_container: {
+          "the name of your database container" => "database"},
+        frontend_container: {
+          "the name of your frontend container" => "frontend"},
+        server_container: {
+          "the name of your server container" => "nginx"},
+        dockerhub_email: {
           "your Docker Hub email" => "your-docker-hub-email"},
-        "DOCKERHUB_USER" => {
+        dockerhub_user: {
           "your Docker Hub username" => "your-docker-hub-user-name" },
-        "DOCKERHUB_ORG" => {
+        dockerhub_org: {
           "your Docker Hub organization name" => "your-docker-hub-org-name"},
-        "DOCKERHUB_PASS" => {
+        dockerhub_password: {
           "your Docker Hub password" => "your-docker-hub-password"},
-        "POSTGRES_USER" => {
+        postgres_user: {
           "your Postgres username" => "postgres"},
-        "POSTGRES_PASSWORD" => {
+        postgres_password: {
           "your Postgres password" => "your-postgres-password"} }
     end
   end
--- lib/roro/cli/rollon.rb
+++ lib/roro/cli/rollon.rb
@@ -7,24 +7,25 @@ module Roro
 
     def rollon
       get_configuration_variables
-      if Dir['./*'].empty?
-        raise Roro::Error.new("Oops -- Roro can't roll itself onto a Rails app if it doesn't exist. Please either change into a directory with a Rails app or generate a new one using '$ roro greenfield'.")
-      end
-      if ask('Configure $stdout.sync for docker?', choices).eql?('y') 
-        config_std_out_true 
-      end
-      if ask('Configure .gitignore for roro?', choices).eql?('y')
-        gitignore_sensitive_files 
-      end 
-      if ask('Add RoRo to your Gemfile?', choices).eql?('y')
-        insert_roro_gem_into_gemfile 
-      end 
-      if ask('Add handsome_fencer-test to your Gemfile?', choices).eql?('y')
-        insert_hfci_gem_into_gemfile 
-      end
-      if ask("Backup 'config/database.yml'?").eql?('y')
-        FileUtils.mv 'config/database.yml', "config/database.yml.backup" 
-      end
+      # if Dir['./*'].empty?
+      #   raise Roro::Error.new("Oops -- Roro can't roll itself onto a Rails app if it doesn't exist. Please either change into a directory with a Rails app or generate a new one using '$ roro greenfield'.")
+      # end
+      # if ask('Configure $stdout.sync for docker?', choices).eql?('y') 
+      #   config_std_out_true 
+      # end
+      # if ask('Configure .gitignore for roro?', choices).eql?('y')
+      #   gitignore_sensitive_files 
+      # end 
+      # if ask('Add RoRo to your Gemfile?', choices).eql?('y')
+      #   insert_roro_gem_into_gemfile 
+      # end 
+      # if ask('Add handsome_fencer-test to your Gemfile?', choices).eql?('y')
+      #   insert_hfci_gem_into_gemfile 
+      # end
+      # if ask("Backup 'config/database.yml'?").eql?('y')
+      #   FileUtils.mv 'config/database.yml', "config/database.yml.backup" 
+      # end
+      copy_roro
     end
   end
 end
