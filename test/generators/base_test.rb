6f5ddbfc318964a0746556217a5cf972034340de
schadenfred <fred.schoeneman@gmail.com>
2020-08-05 01:06:34 -0700

    good stopping poin, most templatable files have been templated.

--- test/generators/base_test.rb
+++ test/generators/base_test.rb
@@ -1,6 +1,5 @@
 require "test_helper"
 
-
 describe Roro::CLI do
   
   Given(:cli) { Roro::CLI.new }
@@ -8,20 +7,23 @@ describe Roro::CLI do
   Given { prepare_destination 'dummy' }
   
   Given(:configurable_env_vars) { {
-    "APP_NAME"=>"dummy", 
-    "RUBY_VERSION"=> /\.\d/, 
-    "SERVER_HOST"=>"ip-address-of-your-server", 
-    "DOCKERHUB_EMAIL"=>"your-docker-hub-email",
-    "DOCKERHUB_USER"=>"your-docker-hub-user-name", 
-    "DOCKERHUB_ORG"=>"your-docker-hub-org-name", 
-    "DOCKERHUB_PASS"=>"your-docker-hub-password", 
-    "POSTGRES_USER"=>"postgres", 
-    "POSTGRES_PASSWORD"=>"your-postgres-password"} }
-  
+    app_name: "dummy", 
+    ruby_version:`ruby -v`.scan(/\d.\d/).first, 
+    server_host: "ip-address-of-your-server", 
+    dockerhub_email: "your-docker-hub-email",
+    dockerhub_user: "your-docker-hub-user-name", 
+    dockerhub_org: "your-docker-hub-org-name", 
+    dockerhub_password: "your-docker-hub-password", 
+    database_container: 'database',
+    frontend_container: 'frontend',
+    server_container: 'nginx',
+    postgres_user: "postgres", 
+    postgres_password: "your-postgres-password"} }
+ 
   Given(:env_vars) { configurable_env_vars.merge(     
-      "DEPLOY_TAG"=>"${CIRCLE_SHA1:0:7}", 
-      "SERVER_PORT"=>"22", 
-      "SERVER_USER"=>"root" ) } 
+    deploy_tag: "${CIRCLE_SHA1:0:7}", 
+    server_port: "22", 
+    server_user: "root" ) } 
     
   describe '.set_from_defaults' do 
  
@@ -35,23 +37,31 @@ describe Roro::CLI do
     Given(:config) { cli.get_configuration_variables } 
     
     Given { env_vars.merge(     
-      "DEPLOY_TAG"=>"${CIRCLE_SHA1:0:7}", 
-      "SERVER_PORT"=>"22", 
-      "SERVER_USER"=>"root" ) } 
+      deploy_tag: "${CIRCLE_SHA1:0:7}", 
+      server_port: "22", 
+      server_user: "root" ) } 
 
     Then { env_vars.each { |key,value| assert_match value, config[key] } }
   end 
   
   describe 'methods' do 
-      
-    # Given { cli.get_configuration_variables }
- 
+    Given { cli.get_configuration_variables }
     describe '.copy_docker_compose' do 
       
       Given { cli.copy_docker_compose }
       
-      Then { assert_file 'docker-compose.yml' }
-    end 
+      Then do 
+        assert_file 'docker-compose.yml' do |c| 
+          assert_match("#{env_vars[:app_name]}:", c )
+          assert_match("dockerfile: docker/containers/#{env_vars[:app_name]}/Dockerfile", c )
+          assert_match("#{env_vars[:app_name]}-data:/usr/src/#{env_vars[:app_name]}", c )
+          assert_match("- #{env_vars[:app_name]}", c)
+          assert_match("- docker/containers/database/development.env", c)
+          assert_match("database:", c)
+          assert_match("- database-data:", c)
+        end
+      end 
+    end
 
     describe ".config_std_out" do 
       
@@ -61,7 +71,6 @@ describe Roro::CLI do
           assert_match("$stdout.sync = true", c )
         end
       end
-      
     end
     
     describe '.gitignore_sensitive_files' do 
@@ -90,9 +99,60 @@ describe Roro::CLI do
       
       Given { cli.copy_roro }
       
-      Then do 
-        assert_directory 'roro' 
-        assert_directory 'roro/containers' 
+      describe 'containers' do 
+        describe 'app_name' do 
+          describe 'Dockerfile' do 
+            Then do 
+              assert_file "roro/containers/app/Dockerfile" do |c| 
+                assert_match "FROM ruby:#{env_vars[:ruby_version]}", c
+                assert_match "maintainer=\"#{env_vars[:dockerhub_email]}", c
+                assert_match "COPY Gemfile* /usr/src/#{env_vars[:app_name]}", c
+                assert_match "WORKDIR /usr/src/#{env_vars[:app_name]}", c
+                assert_match "COPY . /usr/src/#{env_vars[:app_name]}", c
+              end
+            end 
+          end
+          
+          describe 'development.env' do
+            Then do  
+              assert_file 'roro/containers/app/development.env' do |c| 
+                assert_match "DATABASE_HOST=#{env_vars[:database_container]}", c
+              end 
+            end
+          end
+          
+          describe 'test.env' do
+            Then do  
+              assert_file 'roro/containers/app/test.env' do |c| 
+                assert_match "DATABASE_HOST=#{env_vars[:database_container]}", c
+              end 
+            end
+          end
+        end
+        
+        describe 'database_container' do 
+          describe 'development.env' do
+            Then do  
+              assert_file 'roro/containers/database/development.env' do |c| 
+                assert_match "POSTGRES_USER=#{env_vars[:postgres_user]}", c
+                assert_match "POSTGRES_PASSWORD=#{env_vars[:postgres_password]}", c
+                assert_match "POSTGRES_DB=#{env_vars[:app_name] + '_development'}", c
+                assert_match "RAILS_ENV=development", c
+              end 
+            end
+          end
+          
+          describe 'development.env' do
+            Then do  
+              assert_file 'roro/containers/database/test.env' do |c| 
+                assert_match "POSTGRES_USER=#{env_vars[:postgres_user]}", c
+                assert_match "POSTGRES_PASSWORD=#{env_vars[:postgres_password]}", c
+                assert_match "POSTGRES_DB=#{env_vars[:app_name] + '_test'}", c
+                assert_match "RAILS_ENV=test", c
+              end 
+            end
+          end
+        end
       end
     end
     
