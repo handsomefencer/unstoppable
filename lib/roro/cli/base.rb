8063891bf54ba1fe0be68530c94197356b726d02
fred schoeneman <fred.schoeneman@gmail.com>
2020-08-06 17:21:01 -0700

    added quickstart and curated stubs.

--- lib/roro/cli/base.rb
+++ lib/roro/cli/base.rb
@@ -1,7 +1,7 @@
 module Roro
 
   class CLI < Thor
-    
+
     no_commands do
 
       def get_configuration_variables
@@ -14,50 +14,50 @@ module Roro
 
       def set_from_defaults
         @env_hash = configuration_hash
-        @env_hash.map do  |key, hash| 
-          @env_hash[key] = hash.values.last 
+        @env_hash.map do  |key, hash|
+          @env_hash[key] = hash.values.last
         end
         @env_hash
       end
-      
-      def choices 
+
+      def choices
         { default: 'y', limited_to: ["y", "n"] }
       end
-      
+
       def set_interactively
         @env_hash = configuration_hash
         @env_hash.map do |key, prompt|
           answer = ask("Please provide #{prompt.keys.first} or hit enter to accept: \[ #{prompt.values.first} \]")
           @env_hash[key] = (answer == "") ? prompt.values.first : answer
-        end  
-      end  
+        end
+      end
 
       def own_if_required
         system 'sudo chown -R $USER .'
-      end  
-      
-      def copy_docker_compose 
+      end
+
+      def copy_docker_compose
         template "base/docker-compose.yml", 'docker-compose.yml', @env_hash
       end
-      
+
       def config_std_out_true
         prepend_to_file('config/boot.rb', "$stdout.sync = true\n\n")
-      end 
-      
+      end
+
       def gitignore_sensitive_files
         append_to_file ".gitignore", "\nroro/**/*.env\nroro/**/*.key"
       end
-      
-      def copy_circleci 
+
+      def copy_circleci
         directory 'base/circleci', '.circleci', @env_hash
       end
-      
-      def copy_roro 
+
+      def copy_roro
         directory 'base/roro/containers/app', "roro/containers/#{@env_hash[:app_name]}", @env_hash
         directory 'base/roro/containers/database', "roro/containers/#{@env_hash[:database_container]}", @env_hash
         directory 'base/roro/containers/frontend', "roro/containers/#{@env_hash[:frontend_container]}", @env_hash
       end
-  
+
       def insert_roro_gem_into_gemfile
         insert_into_file 'Gemfile', "gem 'roro'", before: "group :development, :test do"
       end
@@ -65,21 +65,22 @@ module Roro
       def insert_pg_gem_into_gemfile
         comment_lines 'Gemfile', /sqlite/
         comment_lines 'Gemfile', /mysql/
-        insert_into_file 'Gemfile', "gem 'pg'", before: "group :development, :test do"
+        comment_lines 'Gemfile', "gem \"pg\""
+        insert_into_file 'Gemfile', "gem 'pg'\n\n", before: "group :development, :test do"
       end
 
       def insert_hfci_gem_into_gemfile
         insert_into_file 'Gemfile', "gem 'handsome_fencer-test'", after: "group :development, :test do"
       end
-      
-      def copy_config_database_yml 
+
+      def copy_config_database_yml
         copy_file "base/config/database.yml", 'config/database.yml', force: true
       end
 
       def copy_base_files
         copy_circleci
-        copy_roro 
-        copy_docker_compose 
+        copy_roro
+        copy_docker_compose
         copy_config_database_yml
       end
     end
