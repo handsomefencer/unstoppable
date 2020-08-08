365e22fa2d26003a3e30f45a1a2372a1874d00d5
schadenfred <fred.schoeneman@gmail.com>
2020-08-07 19:51:53 -0700

    removing stuff from isenberg book.

--- lib/roro/cli/rollon.rb
+++ lib/roro/cli/rollon.rb
@@ -9,21 +9,20 @@ module Roro
     method_option :interactive, desc: "Set up your environment variables as you go."
 
     def rollon
-      if Dir['./*'].empty?
-        raise Roro::Error.new("Oops -- Roro can't roll itself onto a Rails app if it
-          doesn't exist. Please either change into a directory with a Rails app or 
-          generate a new one using '$ roro greenfield'.")
-      end
+      # if Dir['./*'].empty?
+      #   raise Roro::Error.new("Oops -- Roro can't roll itself onto a Rails app if it
+      #     doesn't exist. Please either change into a directory with a Rails app or 
+      #     generate a new one using '$ roro greenfield'.")
+      # end
       get_configuration_variables
-
-      case ask("Configure using the Quickstart, Curated, or HandsomeFencer way?\n\n(1) Quikstart\n\n(2) Curated\n\n(3) HandsomeFencer\n\n", { default: '2', limited_to: %w(1 2 3) })
-      when '1'
-        rollon_as_quickstart
-      when '2'
-        rollon_as_dockerized
-      when '3'
-        rollon_as_roro
-      end
+      # case ask("Configure using the Quickstart, Curated, or HandsomeFencer way?\n\n(1) Quikstart\n\n(2) Curated\n\n(3) HandsomeFencer\n\n", { default: '2', limited_to: %w(1 2 3) })
+      # when '1'
+      #   rollon_as_quickstart
+      # when '2'
+      #   rollon_as_dockerized
+      # when '3'
+      # end
+      rollon_as_dockerized
     end
   end
 end
