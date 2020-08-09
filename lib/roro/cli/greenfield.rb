baace56b8b50c19edec2c4eb18303be397a7851b
schadenfred <fred.schoeneman@gmail.com>
2020-08-09 11:49:29 -0700

    install bundler then bundle

--- lib/roro/cli/greenfield.rb
+++ lib/roro/cli/greenfield.rb
@@ -18,10 +18,10 @@ module Roro
 
       confirm_dependencies
       copy_greenfield_files
-      as_system("docker-compose build web")
-      as_system("docker-compose run web sh")
+      # as_system("docker-compose build web")
+      # as_system("docker-compose run web sh")
       # as_system('docker-compose run web gem install rails --no-document')
-      # as_system('docker-compose run web rails new . --force --no-deps --skip-bundle --skip-webpack-install')
+      as_system("docker-compose run web rails new #{@env_hash[:app_name]} . --force --no-deps --skip-bundle --skip-webpack-install")
       # as_system()
       # rollon_as_dockerized
       # byebug
