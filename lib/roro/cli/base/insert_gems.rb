37fa3befe87f02c13bbc04fc34788af4f0732307
schadenfred <fred.schoeneman@gmail.com>
2020-08-16 09:28:50 -0700

    restore postgres as default db

--- lib/roro/cli/base/insert_gems.rb
+++ lib/roro/cli/base/insert_gems.rb
@@ -6,23 +6,23 @@ module Roro
     no_commands do 
           
       def insert_roro_gem_into_gemfile
-        insert_into_file 'Gemfile', "gem 'roro'", before: "group :development, :test do"
+        insert_into_file 'Gemfile', "gem 'roro'\n\n", before: "group :development, :test do"
       end
 
       def insert_pg_gem_into_gemfile
         comment_lines 'Gemfile', /sqlite/
         comment_lines 'Gemfile', /mysql/
-        insert_into_file 'Gemfile', "gem 'pg'\n", before: "group :development, :test do"
+        insert_into_file 'Gemfile', "gem 'pg'\n\n", before: "group :development, :test do"
       end
 
       def insert_mysql_gem_into_gemfile
         comment_lines 'Gemfile', /sqlite/
         comment_lines 'Gemfile', /pg/
-        insert_into_file 'Gemfile', "gem 'mysql2'\n", before: "group :development, :test do"
+        insert_into_file 'Gemfile', "gem 'mysql2'\n\n", before: "group :development, :test do"
       end
 
       def insert_hfci_gem_into_gemfile
-        insert_into_file 'Gemfile', "gem 'handsome_fencer-test'", after: "group :development, :test do"
+        insert_into_file 'Gemfile', "gem 'handsome_fencer-test'\n\n", after: "group :development, :test do"
       end  
     end
   end 
