0ef5c8e731e657a4d8a163a803d80704b9d94ac1
schadenfred <fred.schoeneman@gmail.com>
2020-08-17 13:59:58 -0700

    added docker entrypoint

--- lib/roro/cli/base/check_dependencies.rb
+++ lib/roro/cli/base/check_dependencies.rb
@@ -34,6 +34,34 @@ module Roro
         system 'docker-compose up'
       end
       
+      def remove_roro_artifacts 
+        appname = Dir.pwd.split('/').last 
+        check_for_clashes = "docker ps --filter name=#{appname}* -aq"
+        byebug
+        unless IO.popen(check_for_clashes).count.eql?(0) 
+          remove_clashes = ["docker ps --filter name=greenfield* -aq | xargs docker stop | xargs docker rm"]
+          volumes = %w(db_data app)
+          volumes.each {|v| remove_clashes << "docker volume rm #{appname}_#{v}"}
+          question = [
+            "\n\nWe found some container and volume artifacts which may clash",
+            "with RoRo moving forward. You can verify their existence in a ",
+            "separate terminal with:",
+            "\n\t$ #{check_for_clashes}'\n", 
+            "You can remove these artifacts with something like:\n" 
+          ]
+          remove_clashes.each { |c| question << "\t$ #{c}"}
+          question << "\nWould you like RoRo to attempt to remove them for you?"
+          question = question.join("\n")
+      
+          prompt = [question]
+          choices = {'y' => 'Yes', 'n' => 'No'}
+          choices.each { |k,v| prompt << "(#{k}) #{v.to_s}" }
+          answer = ask((prompt.join("\n\n") + "\n\n"), 
+          default: 'y', limited_to: choices.keys)
+          remove_clashes.each {|c| system c }
+        end
+      end
+    
       def confirm_dependency(options)
         msg = []
         msg << ""
--- lib/roro/cli/rollon.rb
+++ lib/roro/cli/rollon.rb
@@ -12,7 +12,10 @@ module Roro
     def rollon
       confirm_directory_not_empty
       confirm_dependencies
+      remove_roro_artifacts 
+
       configure_for_rollon
+      
       copy_roro_files
       generate_roro_config
       startup_commands
@@ -30,7 +33,7 @@ module Roro
       
       def copy_roro_files 
         configure_database  
-        directory 'roro/', './', @config.app
+        directory 'roro', './', @config.app
         template 'base/Dockerfile.tt', 'roro/containers/app/Dockerfile', @config.app
         take_thor_actions
       end
@@ -39,7 +42,7 @@ module Roro
         @config.thor_actions.each {|k, v| eval(k) if v.eql?('y') }  
       end
               
-      def startup_commands 
+      def startup_commands
         system 'docker-compose build'
         system 'docker-compose run web bundle'
         system 'docker-compose run web bin/rails webpacker:install'
@@ -48,6 +51,34 @@ module Roro
         system 'docker-compose run web bin/rails db:migrate'
         system 'docker-compose up'
       end
+      
+      def confirm_directory_empty 
+        confirm = confirm_dependency({
+          system_query: "ls -A",
+          warning: "this is not an empty directory. Roro will not greenfield a new Rails app unless either a) the current directory is empty or b) you run greenfield with the --force flag",
+          suggestion: "$ roro greenfield --force",
+          conditional: "Dir.glob('*').empty?" }) 
+        confirm || true
+      end
+
+    
+      def confirm_dependency(options)
+        msg = []
+        msg << ""
+        msg << delineator
+        msg << "It looks like #{options[:warning]}. The following bash command returns false:"
+        msg << "\t$ #{options[:system_query]}"
+        msg << "Please try these instructions:"
+        msg << ("\t" + options[:suggestion])
+        msg << delineator
+        conditional = options[:conditional] ? eval(options[:conditional]) : system(options[:system_query])
+        if conditional == false
+          raise(Roro::Error.new(msg.join("\n\n")))
+        else 
+          true 
+        end
+      end
+      
     end
   end
 end
