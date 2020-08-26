09baf3a58db522186a21b0d5c31fed7c6055408b
schadenfred <fred.schoeneman@gmail.com>
2020-08-26 13:24:51 -0700

    most of changes from hf rolled in.

--- test/cli/rollon/stories/rails/ci_cd/circleci_test.rb
+++ test/cli/rollon/stories/rails/ci_cd/circleci_test.rb
@@ -26,28 +26,6 @@ describe "Story::Rails::WithCICD" do
       Given(:structure) { YAML.load_file('.circleci/config.yml')}
           
       Then { 
-        assert_includes structure.keys, 'version'  
-        assert structure['jobs']['build'] 
-        assert structure['jobs']['test'] 
-        assert structure['workflows']['build-and-test'] 
-      }
-    end 
-    And  { assert_directory "roro/containers/frontend" } 
-  end
-
-    
-    
-  
-  
-  describe '.circleci/config.yml' do 
-    
-    Given(:file) { '.circleci/config.yml' }
-    
-    describe 'must have the correct structure' do
-      
-      Given(:structure) { YAML.load_file('.circleci/config.yml')}
-      
-      Then { 
         assert_includes structure.keys, 'version' 
         assert_includes structure.keys, 'jobs' 
         assert_includes structure.keys, 'workflows' 
@@ -56,42 +34,5 @@ describe "Story::Rails::WithCICD" do
         assert structure['workflows']['build-and-test'] 
       }
     end 
-    
-    #     Given(:expected) { r = rubies.first; [
-      #       "version: 2.1",
-      #       "- run: RUBY_VERSION=#{r} docker-compose build ruby_gem",
-      #       "- run: RUBY_VERSION=#{r} docker-compose up -d --force-recreate",
-      #       "- run: RUBY_VERSION=#{r} docker-compose run ruby_gem bundle exec rake",
-      #     ]}
-      
-      #     Then { assert_file(file) {|c| expected.each {|e| assert_match e, c } }}
-      #   end
-      
-      #   Given(:rubies) { config.app[ 'rubies' ] }
-      
-      #   describe 'must have rubies set' do 
-      
-      #     Then { assert_equal ['2.7', '2.6', '2.5'], rubies } 
-      #   end
-      
-      #   describe 'roro/ci.env' do 
-      
-      #     Given(:file) { 'roro/containers/ruby_image/ci.env' }
-      #     Given(:expected) { "export RUBYGEMS_API_KEY=#{rubygems_api_key}"}
-      
-      #     Then { assert_file(file) {|c| assert_match expected, c} } 
-      #   end
-      
-      #   describe 'roro/containers/ruby_gem/Dockerfile' do 
-      
-      #     Given(:file) { "roro/containers/ruby_image/Dockerfile" }    
-      
-      #     Then { assert_file(file) { |c| assert_match "FROM $RUBY_IMAGE", c } }  
-      #   end
-      
-      #   describe 'docker-compose.yml' do 
-      
-      #     Then { assert_file 'docker-compose.yml' } 
-      #   end
-    end
   end
+end
