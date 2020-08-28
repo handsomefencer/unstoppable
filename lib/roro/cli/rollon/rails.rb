c5883328c995cb737502e20f06d3c0226f5e7c3d
schadenfred <fred.schoeneman@gmail.com>
2020-08-28 14:03:03 -0700

    remove stories namespace, moved utility methods into rollon.rb to keep them at same nesting structure expected.

---
+++ lib/roro/cli/rollon/rails.rb
@@ -0,0 +1,47 @@
+require 'roro/cli/rollon/stories'
+
+module Roro
+
+  class CLI < Thor
+        
+    desc "rollon::rails", "Generates files for and makes changes to your app 
+      so it can run using Docker containers."
+    method_option :interactive, desc: "Set up your environment variables as 
+      you go."
+    map "rollon::rails" => "rollon_rails"
+    
+    def rollon_rails(*args) 
+      confirm_directory_not_empty
+      confirm_dependencies
+      configure_for_rollon
+      
+      copy_roro_files
+      generate_config
+      startup_commands
+    end
+    
+    no_commands do
+     
+      def copy_roro_files
+        template 'rails/.circleci/config.yml.tt', './.circleci/config.yml' 
+        configure_database  
+        directory 'roro', './', @config.app
+        template 'base/dotenv', './.env', @config.app
+        directory 'rails/roro', './roro', @config.app
+        take_thor_actions
+      end
+                 
+      def startup_commands
+        success_msg = "'\n\n#{'*' * 5 }\n\nYour Rails app is available at http://localhost:3000/'\n\n#{'*' * 5 }"
+        system 'docker-compose build'
+        system 'docker-compose run web bundle'
+        system 'docker-compose run web bin/rails webpacker:install'
+        system 'docker-compose run web bin/rails yarn:install'
+        system 'docker-compose run web bin/rails db:create'
+        system 'docker-compose run web bin/rails db:migrate'
+        system 'docker-compose up -d'
+        system "docker-compose run web echo '\n\nYour Rails app is available at http://localhost:3000/'"
+      end
+    end
+  end
+end
---
+++ test/cli/rollon/rails/database/with_postgres_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+describe Roro::CLI do
+
+  Given { prepare_destination 'rails/603' }
+  Given(:subject) { Roro::CLI.new } 
+  Given(:config) { subject.configure_for_rollon }
+  
+  describe '.rollon with postgresql' do 
+
+    Given { config.thor_actions['configure_database'] = 'p' }
+
+    Given { subject.copy_roro_files }
+
+    describe 'pg gem in gemfile' do 
+  
+      Then do 
+        assert_file 'Gemfile' do |c| 
+          assert_match("gem 'pg'", c)
+        end
+      end
+    end
+    
+    describe "config/database.yml" do  
+        
+      Then do 
+        assert_file 'config/database.yml' do |c| 
+          assert_match("<%= ENV.fetch('DATABASE_HOST') %>", c)
+          assert_match("<%= ENV.fetch('POSTGRES_USER') %>", c)
+          assert_match("<%= ENV.fetch('POSTGRES_PASSWORD') %>", c)
+          assert_match("<%= ENV.fetch('POSTGRES_DB') %>", c)
+        end
+      end
+    end
+    
+    describe 'env_files' do 
+      Given(:vars) { config.app['postgresql_env_vars'] }
+      %w(development production test staging ci).each do |env| 
+        
+        Then do 
+          assert_file( "roro/containers/database/#{env}.env" ) do |c| 
+            assert_match "POSTGRES_USER=#{vars['postgres_user']}", c 
+            assert_match "POSTGRES_PASSWORD=#{vars['postgres_password']}", c 
+            assert_match "POSTGRES_DB=#{config.app['main_app_name'] + "_" + env}", c 
+            assert_match "RAILS_ENV=#{env}", c 
+          end 
+        end
+      end
+      And { assert_nil config.app['rails_env'] }
+    end
+
+    describe 'docker-compose.yml' do
+      
+      Given(:expected) { yaml_from_template('stories/with_postgresql/_service.yml')}
+      
+      Then do
+        assert_file "docker-compose.yml" do |c| 
+          assert_match expected, c 
+        end
+      end 
+    end 
+  end
+end
