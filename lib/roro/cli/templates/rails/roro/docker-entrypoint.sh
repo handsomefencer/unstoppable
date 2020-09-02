a3bc371c2b56af8a17723dabf00dc4c34964859b
schadenfred <fred.schoeneman@gmail.com>
2020-09-01 21:48:27 -0700

    move to alpine to save time

--- lib/roro/cli/templates/rails/roro/docker-entrypoint.sh
+++ lib/roro/cli/templates/rails/roro/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-if [ -f tmp/pids/server.pid ]; then
-rm tmp/pids/server.pid
-fi
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /usr/src/app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
