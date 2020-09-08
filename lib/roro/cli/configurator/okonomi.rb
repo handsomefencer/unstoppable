e6d1c3e20e4dd8c932c2031234f93d878deb1954
fred schoeneman <fred.schoeneman@gmail.com>
2020-09-08 16:26:02 -0700

    not any further along.

--- lib/roro/cli/configurator/okonomi.rb
+++ lib/roro/cli/configurator/okonomi.rb
@@ -2,48 +2,52 @@ require 'yaml'
 require 'json'
 module Roro
   class Configurator < Thor::Shell::Basic
-    
+
     def default_story(story, location=nil)
-      default_story = {}
-      story.each do |key, value| 
-        location = location ? (location + '/' + key.to_s ) : key.to_s
+      story ||= {}
+      story.each do |key, value|
+        location ||= key.to_s
         filedir = File.dirname(__FILE__) + "/#{location}"
         filepath = "#{filedir}.yml"
-        layer = get_layer(filepath) 
-        case 
-        when value.is_a?(Array)
-          value.each {|v| build_layers(v, location) }
-        when value.empty?
-          if layer[:story]
-            byebug
-            default_story[:key] = layer[:story]
-            # byebug
-          else  
-          end 
-        # when value.is_a?(Hash) 
-        #   default_story = 
-          
-          # build_default_story(value, location) 
+        byebug if !File.exist? filepath# byebug
+        layer = get_layer(filepath)
+        # case
+        # if value.is_a?(Array)
+        # value.each {|v| build_layers(v, location) }
+        # when value.empty?
+        if layer[:story]
+          story[key] = layer[:story]
+          filename = story[key].keys.first.to_s# byebug
+          location = location ? (location + '/' + filename) : filename
+          default_story(story, location)
+        elsif layer[:stories]
+          byebug
+          # else
+          # end
+        # when value.is_a?(Hash)
+        #   default_story =
+
+          # build_default_story(value, location)
         end
       end
-      default_story 
-    end 
-    
-    def take_order 
-      
-      ask_questions  
+      story
     end
-    
+
+    def take_order
+
+      ask_questions
+    end
+
     def ask_questions
-      @structure[:choices].each do |key, value| 
+      @structure[:choices].each do |key, value|
         @structure[:intentions][key] = ask_question(value)
       end
     end
-    
-    def ask_question(question) 
+
+    def ask_question(question)
       prompt = question[:question]
       default = question[:default]
-      choices = question[:choices].keys 
+      choices = question[:choices].keys
       answer = ask(prompt, default: default, limited_to: choices)
       answer
     end
--- test/cli/configurator/okonomi_test.rb
+++ test/cli/configurator/okonomi_test.rb
@@ -1,30 +1,28 @@
 require "test_helper"
 
 describe Roro::Configurator do
-  
+
   Given { greenfield_rails_test_base }
   Given(:asker) { Thor::Shell::Basic.any_instance }
   Given { asker.stubs(:ask).returns('y') }
   Given(:options) { nil }
   Given(:config) { Roro::Configurator.new(options) }
 
-  
-  describe 'default story' do 
+
+  describe 'default story' do
     Then { assert_equal 'blah', config.default_story({stories: {}}) }
   end
 
-
-  
   describe 'take_order' do
-    
+
     Given(:questions)  { config.structure[:choices] }
     Given(:intentions) { config.structure[:intentions] }
-    
+
     Given { config.stubs(:ask).returns('n')}
     Given { config.take_order }
-    
+
     Then { intentions.each { |key, value| assert_equal 'n', value  } }
   end
-  
+
   # describe ''
 end
