bb225cc89aecbdad9a368f729cca1db458404ae3
schadenfred <fred.schoeneman@gmail.com>
2020-09-14 08:53:11 -0700

    mocked out rollon methods, start generate::story cli optioin

--- lib/roro/cli/generate/story.rb
+++ lib/roro/cli/generate/story.rb
@@ -1,17 +1,18 @@
 module Roro
   class CLI < Thor
     
-    desc "generate::config", "Generate a config file at .roro_config.yml" 
-    map "generate::config" => "generate_config"
+    desc "generate::story", "Generate a config file at .roro_config.yml to use later" 
+    map "generate::story" => "generate_story"
     
-    def generate_config
-      @config ||= Roro::Configurator::Configuration.new(options)
-      configuration = {
-        env_vars: @config.env.keys,
-        intentions: @config.intentions,
-        story: @config.structure['story']
-      }
-      create_file ".roro_configurator.yml", @config.env.to_yaml 
+    def generate_story
+      
+      @config ||= Roro::Configuration.new(options)
+      # configuration = {
+      #   env_vars: @config.env.keys,
+      #   intentions: @config.intentions,
+      #   story: @config.structure['story']
+      # }
+      # create_file ".roro_configurator.yml", @config.env.to_yaml 
     end
   end 
 end
