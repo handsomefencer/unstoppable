0bcc47a90622fa9bbf3ad1183000a472e817b818
Fred.schoeneman@gmail.com <fred.schoeneman@gmail.com>
2021-06-05 21:10:49 -0700

    spacing

--- test/roro/configurator/configurator_test.rb
+++ test/roro/configurator/configurator_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 describe Roro::Configurator::Configurator do
@@ -7,26 +9,22 @@ describe Roro::Configurator::Configurator do
 
   describe '#sanitize(options' do
     context 'when key is a string' do
-
       When(:options) { { 'key' => 'value' } }
       Then { assert config.options.keys.first.is_a? Symbol }
     end
 
     context 'when value is a' do
       context 'string' do
-
         When(:options) { { 'key' => 'value' } }
         Then { assert config.options.values.first.is_a? Symbol }
       end
 
       context 'when value is an array' do
-
         When(:options) { { 'key' => [] } }
         Then { assert config.options.values.first.is_a? Array }
       end
 
       context 'when value is an array of hashes' do
-
         When(:options) { { 'key' => [{ 'foo' => 'bar' }] } }
         Then { assert_equal :bar, config.options[:key][0][:foo] }
       end
@@ -34,7 +32,6 @@ describe Roro::Configurator::Configurator do
   end
 
   describe '#structure' do
-
     Then { assert_includes config.structure.keys, :intentions }
     And  { assert_includes config.structure.keys, :choices }
     And  { assert_includes config.structure.keys, :env_vars }
