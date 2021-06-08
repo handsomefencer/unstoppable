dc5c69c62efd7856552aa68d22937c4d228f0917
Fred.schoeneman@gmail.com <fred.schoeneman@gmail.com>
2021-06-08 11:41:29 -0700

    before moving cya stuff to okonomi

--- lib/roro/configurators/omakase.rb
+++ lib/roro/configurators/omakase.rb
@@ -3,7 +3,6 @@
 module Roro
   module Configurators
     class Omakase < Roro::Configurators::Configurator
-
       def junbi(location = nil)
         location ||= "#{Roro::CLI.story_root}/entrees"
         hash = {}
@@ -24,37 +23,9 @@ module Roro
         end
       end
 
-      # def get_layer(filedir)
-      #   filepath = "#{filedir}.yml"
-      #   key = filepath.split('/').last
-      #   error_msg = "Cannot find that story #{key} at #{filepath}. Has it been written?"
-      #   raise Roro::Error, error_msg unless File.exist?(filepath)
-      #
-      #   json = JSON.parse(YAML.load_file(filepath).to_json, symbolize_names: true)
-      #   json || raise(Roro::Story::StoryMissing, "Is #{filepath} empty?")
-      # end
+      def choose_your_adventure
 
-      # def story_map(story = 'stories')
-      #   array ||= []
-      #   loc = Roro::CLI.story_root + "/#{story}"
-      #   validate_story(loc)
-      #   stories = Dir.glob("#{loc}/*.yml")
-      #   stories.each do |ss|
-      #     name = ss.split('/').last.split('.yml').first
-      #     array << { name.to_sym => story_map([story, name].join('/')) }
-      #   end
-      #   array
-      # end
-
-      # def validate_story(story)
-      #   scenes = get_layer(story)[:stories]
-      #   case scenes
-      #   when String
-      #     File.exist?("#{story}#{scenes}.yml")
-      #   when Array
-      #     scenes.each { |scene| validate_story("#{story}/#{scene}") }
-      #   end
-      # end
+      end
     end
   end
 end
