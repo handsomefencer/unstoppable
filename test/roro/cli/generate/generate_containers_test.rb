d00a158864da2f541077b7ead7d90e4ce9d2a57f
Fred.schoeneman@gmail.com <fred.schoeneman@gmail.com>
2021-09-28 11:50:47 -0700

    Suppresses stdout in tests.

--- test/roro/cli/generate/generate_containers_test.rb
+++ test/roro/cli/generate/generate_containers_test.rb
@@ -3,30 +3,27 @@
 require 'test_helper'
 
 describe 'Roro::CLI#generate_containers' do
-  let(:subject)              { Roro::CLI.new }
-  let(:workbench)            { 'roro' }
-  let(:default_apps)         { %w[backend database frontend] }
-  let(:containers)           { nil }
-  let(:generate) { quiet { subject.generate_containers(*containers) } }
+  let(:subject)      { Roro::CLI.new }
+  let(:workbench)    { 'roro' }
+  let(:default_apps) { %w[backend database frontend] }
+  let(:containers)   { nil }
+  let(:generate)     { quiet { subject.generate_containers(*containers) } }
 
   describe 'when non-directory sibling exists in workbench' do
-
     Given { insert_dummy_env 'dummy.env' }
     Given { generate }
     Then  { refute_file 'roro/containers/dummy' }
   end
 
   describe 'when hidden sibling exists in workbench' do
-    let(:workbench) { %w[roro .dotfolder] }
-
+    When(:workbench) { %w[roro .dotfolder] }
     Given { generate }
     Then  { refute_file 'roro/containers/.dotfolder' }
   end
 
   context 'when no sibling folders and when' do
     context 'no containers supplied must generate default containers' do
-      let(:containers) { nil }
-
+      When(:containers) { nil }
       Given { generate }
       Then  { assert_directory 'roro/containers/backend/scripts' }
       And   { assert_directory 'roro/containers/database/env' }
@@ -34,9 +31,7 @@ describe 'Roro::CLI#generate_containers' do
     end
 
     context 'containers supplied must generate specified containers' do
-      let(:workbench)    { 'roro' }
-      let(:containers) { %w[pistil stamen database] }
-
+      When(:containers) { %w[pistil stamen database] }
       Given { generate }
       Then  { assert_directory 'roro/containers/frontend/scripts' }
     end
@@ -46,8 +41,7 @@ describe 'Roro::CLI#generate_containers' do
     let(:workbench) { %w[roro pistil stamen] }
 
     context 'when no containers supplied must generate sibling containers' do
-      let(:containers) { nil }
-
+      When(:containers) { nil }
       Given { generate }
       Then  { assert_directory 'roro/containers/pistil/env' }
       And   { assert_directory 'roro/containers/pistil/scripts' }
