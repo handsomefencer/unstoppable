79c0d427777e0dd716a88d0d945540653b70a55a
Jane <jane.doe@example.com>
2021-10-29 18:12:23 -0700

    for dev

--- lib/roro/configurators/adventure_writer.rb
+++ lib/roro/configurators/adventure_writer.rb
@@ -12,6 +12,7 @@ module Roro
 
         def write(buildenv, storyfile)
           @env = buildenv
+          @env[:force] = true
           actions = read_yaml(storyfile)[:actions]
           unless actions.nil?
             self.source_paths << "#{stack_parent_path(storyfile)}/templates"
