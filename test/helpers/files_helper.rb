9abdaf4d8fc497982975b7ed7ad253ed4b51a45b
Fred Schoeneman <fred.schoeneman@gmail.com>
2021-11-24 13:02:20 -0800

    Simplifies helper method. Adds dummy production.rb file for v6_1 test.

--- test/helpers/files_helper.rb
+++ test/helpers/files_helper.rb
@@ -10,8 +10,10 @@ module Roro
         end
 
         def assert_file_match_in(file_matcher, files)
-          assert file_match_in_files?(file_matcher, files),
-                 "'...#{file_matcher}' doesn't match any files in: #{files}"
+          msg = "'...#{file_matcher}' doesn't match any files in: #{files}"
+          assert(files.any? {|file| file.match file_matcher }, msg )
+          # assert file_match_in_files?(file_matcher, files),
+          #        "'...#{file_matcher}' doesn't match any files in: #{files}"
         end
 
         def assert_file(file, *contents)
