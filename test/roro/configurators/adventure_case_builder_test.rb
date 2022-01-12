55c4f7db4f65e2f37d120740f48fed71ac380eba
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-12 10:37:42 -0800

    refactor to hadle it all inside tests.

--- test/roro/configurators/adventure_case_builder_test.rb
+++ test/roro/configurators/adventure_case_builder_test.rb
@@ -3,13 +3,30 @@
 require 'test_helper'
 
 describe AdventureCaseBuilder do
-  Given(:stack_loc)    { "#{Roro::CLI.stacks}" }
-  Given(:case_builder) { AdventureCaseBuilder.new(stack_loc) }
-  Given(:expected)     { read_yaml("#{Dir.pwd}/mise/logs/matrix_cases.yml") }
+  Given(:stack_loc)     { Roro::CLI.stacks }
+  Given(:case_builder)  { AdventureCaseBuilder.new(stack_loc) }
+  Given(:matrixes_path) { 'test/fixtures/matrixes' }
+  Given(:expected)      { read_yaml("#{Dir.pwd}/#{file}") }
+  Given(:file)          { "#{matrixes_path}/#{matrix}.yml" }
+  Given(:content)       { read_yaml("#{Dir.pwd}/#{file}") }
+
 
   describe '#build_cases' do
     When(:expected) { [:inflections, :stacks, :stories] }
     Then { assert_equal expected, case_builder.build_cases.keys }
+  end
+
+  describe '#generate_fixture_cases' do
+    Given(:matrix) { 'cases' }
+    Given { case_builder.generate_fixture_cases }
+
+    describe 'must generate file' do
+      Then { assert_file file }
+
+      describe 'must generate file' do
+        Then { assert_equal 'blah', expected }
+      end
+    end
   end
 
   describe '#case_from_path' do
---
+++ test/tasks/fixtures/generate_test.rb
@@ -0,0 +1,54 @@
+# # frozen_string_literal: true
+#
+# require 'test_helper'
+# require 'rake'
+#
+# describe '' do
+#   Given(:matrixes_path) { 'test/fixtures/matrixes' }
+#   Given(:workbench)     { matrixes_path }
+#   Given(:file)          { "#{matrixes_path}/#{matrix}.yml" }
+#   Given(:content)       { read_yaml("#{Dir.pwd}/#{file}") }
+#
+#   describe ':cases' do
+#     Given(:matrix)  { 'cases' }
+#     Given(:execute) { capture_subprocess_io {
+#       Rake::Task['fixtures:generate:cases'].execute
+#     }}
+#
+#     context 'when task has not run' do
+#       Then { refute_file file }
+#     end
+#
+#     context 'when task is run' do
+#       describe 'must create cases file'do
+#         Given { execute }
+#         Then  { assert_file file }
+#       end
+#
+#       describe 'must provide cli output' do
+#         Then { assert_match 'Creating', execute.first }
+#         And  { assert_match 'Created', execute.first }
+#       end
+#
+#       describe 'must return correct' do
+#         Given { execute }
+#
+#         describe 'number of cases' do
+#           Then  { assert_equal 33, content.size }
+#         end
+#
+#         describe 'simple case' do
+#           Then { assert_includes content, [1,1] }
+#         end
+#
+#         describe 'intermediate case' do
+#           Then { assert_includes content, [3,1,2,1] }
+#         end
+#
+#         describe 'advanced case' do
+#           Then { assert_includes content, [3,2,1,1,2,2,1] }
+#         end
+#       end
+#     end
+#   end
+# end
