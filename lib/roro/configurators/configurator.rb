0785d0918530a66eca28840b2a9eacd31efee890
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-21 10:34:10 -0800

    Adds RORO_ENV for testing.

--- lib/roro/configurators/configurator.rb
+++ lib/roro/configurators/configurator.rb
@@ -98,7 +98,7 @@ module Roro
       def write_story
         @manifest.each do |m|
           @writer.write(@structure, m)
-          copy_stage_dummy(m)
+          copy_stage_dummy(m) unless ENV['RORO_ENV']
         end
       end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+ENV['RORO_ENV'] ||= 'test'
 
 require 'byebug'
 require 'fileutils'
