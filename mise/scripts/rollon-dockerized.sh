75199bc38cfa98be654ac5068bd6d99c2f283726
Jane <jane.doe@example.com>
2022-01-20 23:09:07 -0800

    adds 6_1 back

--- mise/scripts/rollon-dockerized.sh
+++ mise/scripts/rollon-dockerized.sh
@@ -15,46 +15,12 @@ DST=~/work/opensource/gems/sandbox
   if [[ $PWD = !$DST ]]; then
     echo "Wrong directory"
     exit
+  else
+    docker-compose down
+    sudo docker run \
+      -v $PWD:/home/schadenfred \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -u 0 \
+      -it roar:latest roro rollon
   fi
-
-  docker-compose down
-
-  sudo rm -rf ./* ./.*
-
-  sudo docker run \
-    -v $PWD:/home/schadenfred \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -u 0 \
-    -it roar:latest roro rollon
-#    -it roar:latest roro rollon
-
-
 )
-
-  #  printf "2\n4\na$var\n" | roro rollon
-
-#    sudo rm -rf ./* ./.*
-
-#(
-#  cd $SRC
-#  sudo docker-compose build
-#)
-
-
-
-#(
-#  cd ../../roro &&
-#  echo $SRC
-#  echo $DST
-#  sudo docker-compose build
-#
-#)
-##docker system prune --volumes
-##(cd ~/work/opensource/gems/workbench_roro/roro && docker-compose build)
-#
-#sudo docker run \
-#-v $PWD:/home/schadenfred \
-#-v /var/run/docker.sock:/var/run/docker.sock \
-#-u 0 \
-#-it roar:latest roro rollon
-#
