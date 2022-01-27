c815375bf229a89892f9d72bd67a3a4b5965b952
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-26 16:46:40 -0800

    automated

--- mise/scripts/rollon/run.sh
+++ mise/scripts/rollon/run.sh
@@ -4,6 +4,7 @@ sudo docker run \
   -v $PWD:/home/schadenfred \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -u 0 \
-  -t roar:latest roro rollon
-#  -t roar:latest printf "1\\n3\\n1\\n2\\n2\\n1\\na\\n$var" | roro rollon
+  -it roar:latest roro rollon
+
+
 
