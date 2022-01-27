8487c963508b3cef0ce6ca9355b6784b8c722d0e
Jane <jane.doe@example.com>
2022-01-26 23:12:08 -0800

    automated

--- mise/scripts/rollon/down.sh
+++ mise/scripts/rollon/down.sh
@@ -3,7 +3,7 @@
 cd $DST
 
 docker-compose down
-docker ps --filter name=sandbox* -a -q | xargs docker stop | xargs docker rm -f
-docker volume ls --filter name=sandbox* -q | xargs docker volume rm -f
+docker ps --filter name=sandbox* -a -q      | xargs docker stop | xargs docker rm -f
+docker volume ls --filter name=sandbox* -q  | xargs docker volume rm -f
 docker network ls --filter name=sandbox* -q | xargs docker network rm
-docker image ls sandbox* -q | xargs docker rmi -f
+docker image ls sandbox* -q                 | xargs docker rmi -f
