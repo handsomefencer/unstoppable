7b5f5cf99f13a37ae2989ec7e14cc3d17d39e191
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-26 18:07:07 -0800

    automated

--- mise/scripts/rollon/build_gem.sh
+++ mise/scripts/rollon/build_gem.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd $SRC
-rm -rf pkg/*
-rm roro-0*
 
 docker-compose build
 
--- mise/scripts/rollon/git_commit.sh
+++ mise/scripts/rollon/git_commit.sh
@@ -2,4 +2,7 @@
 
 cd $SRC
 #git checkout -b roller
+rm -rf pkg/*
+rm roro-0*
+
 git commit -am 'automated'
