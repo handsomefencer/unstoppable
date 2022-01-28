aa16df019611da2d1b01d70eac0ab60586413daf
Jane <jane.doe@example.com>
2022-01-28 14:32:56 -0800

    before removing dummies

---
+++ rakelib/test/annotate.rake
@@ -0,0 +1,12 @@
+namespace :test do
+  desc 'Annotate adventure tests'
+  task :annotate do
+
+  end
+
+  private
+
+  def test_files
+
+  end
+end
---
+++ test/fixtures/dummies/test_annotate/lib/roro/stacks/unstoppable_developer_styles/omakase/frameworks/wordpress/test/stage_one/stage_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+describe 'omakase wordpress' do
+  Given { skip }
+  Given(:workbench)  { 'empty' }
+  Given(:cli)        { Roro::CLI.new }
+  Given(:overrides)  { [''] }
+  Given(:rollon)     {
+    copy_stage_dummy(__dir__)
+    stubs_adventure(__dir__)
+    stub_overrides
+    stub_run_actions
+    cli.rollon
+  }
+
+  Given { quiet { rollon } }
+
+  describe 'must have docker-compose.yml file' do
+    Then  { assert_file 'docker-compose.yml' }
+  end
+
+  describe 'must have a mise en place' do
+    Then  { assert_file 'mise/mise.roro' }
+  end
+
+  describe 'must have correct containers' do
+    Then  { assert_file 'mise/containers/wordpress' }
+  end
+
+  describe 'must have correct variables in files' do
+    Then  { assert_file 'mise/env/base.env', /WORDPRESS_IMAGE=wordpress/ }
+  end
+end
