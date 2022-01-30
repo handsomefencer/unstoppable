19b47d3e465eaf43361102aa6001bb35281b0c62
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-30 15:58:31 -0800

    simplifies generate_annotations test

--- test/roro/cli/generate/generate_annotations_test.rb
+++ test/roro/cli/generate/generate_annotations_test.rb
@@ -9,32 +9,11 @@ describe 'Roro::CLI#generate_annotations' do
   Given(:space)          { "#{base}/okonomi/languages/ruby/frameworks/rails" }
   Given(:file)           { "#{space}/versions/v6_1/test/0/_test.rb"}
 
-  Given(:assert_content) { -> (c) { assert_file( file, /#{c}/ )} }
-
-  Given(:blocktext) do
-    <<~HEREDOC
-    describe 'adventure::rails_v6_1::0::sqlite & ruby_v2_7' do
-      Given(:workbench)  { }
-      Given { @rollon_loud    = true }
-      Given { @rollon_dummies = false }
-      Given { rollon(__dir__) }
-    
-      describe
-    HEREDOC
-  end
+  Given(:expected) { /describe 'adventure::rails-v6_1::0 sqlite & ruby-v2_7'/ }
 
   Given { subject.generate_annotations }
 
   describe 'after generate' do
-    focus
-    Then  { assert_content[blocktext] }
-  end
-
-  describe '#adventure_test_files' do
-    Then { assert_includes subject.adventure_test_files, file, expected }
-  end
-
-  describe '#adventure_description(stack)' do
-    Then { assert_match subject.adventure_description(file), expected }
+    Then  { assert_file file, expected }
   end
 end
