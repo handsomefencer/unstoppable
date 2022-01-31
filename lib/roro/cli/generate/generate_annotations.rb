7e4e86c6abdb77821b5b2d1c252d34d5f6b06fcb
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-01-31 15:56:33 -0800

    mostly there.

--- lib/roro/cli/generate/generate_annotations.rb
+++ lib/roro/cli/generate/generate_annotations.rb
@@ -15,15 +15,9 @@ module Roro
         stack_location = file.split('lib/roro/stacks').last
         description = adventure_description(stack_location)
         gsub_file file, /\ndescribe (.*)[\s\S]*\n\s\sdescribe/ do |match|
-          [
-            "\ndescribe '#{description}' do",
-            "\s\sGiven(:workbench)  { }",
-            "\s\sGiven { @rollon_loud    = false }",
-            "\s\sGiven { @rollon_dummies = false }",
-            "\s\sGiven { rollon(__dir__) }",
-            "\n\s\sdescribe"
-          ].join("\n")
+          # describe_dummy_file(dummy_file_name)
         end
+
       end
     end
 
@@ -43,6 +37,181 @@ module Roro
         getsome.delete(story)
         getsome.map! { |i| i[1].eql?('versions') ? "#{i[0]}-#{i[-1]}" : i.last }
         "adventure::#{getsome.shift}::#{index} #{getsome.join(' & ')}"
+      end
+
+      def description_helper(adventure_test)
+
+      end
+
+
+      def describe_outmost(description)
+        [
+          "\ndescribe '#{description}' do",
+          "\s\sGiven(:workbench)  { }",
+          "\s\sGiven { @rollon_loud    = false }",
+          "\s\sGiven { @rollon_dummies = false }",
+          "\s\sGiven { rollon(__dir__) }",
+          "\s\sGiven(:assert_content) { -> (c) { assert_file file, c }",
+          "\n\s\sdescribe"
+        ].join("\n")
+      end
+
+      def describe_dummy_file_with(dummy_file)
+        [
+          "\ndescribe '#{dummy_file}' do",
+          "\s\sGiven(:workbench)  { }",
+          "\s\sGiven { @rollon_loud    = false }",
+          "\s\sGiven { @rollon_dummies = false }",
+          "\s\sGiven { rollon(__dir__) }",
+          "\s\sGiven(:assert_content) { -> (c) { assert_file file, c }",
+          "\n\s\sdescribe"
+        ].join("\n")
+
+      end
+
+      def describe_actions(test_file)
+
+      end
+
+      def describe_after_actions(test_file)
+        [
+          "\n\s\sdescribe 'must have generated' do",
+
+        ].join("\n")
+      end
+
+      def describe_before_actions(test_file)
+        story = "#{stack_name(File.expand_path('../../..', test_file))}.yml"
+        [
+          "\n\s\sdescribe ':actions in #{story}' do",
+          "\n\s\s\s\sdescribe 'before' do",
+          "\n\s\s\s\send",
+          "\s\sGiven(:workbench)  { }",
+          "\s\sGiven { @rollon_loud    = false }",
+          "\s\sGiven { @rollon_dummies = false }",
+          "\s\sGiven { rollon(__dir__) }",
+          "\s\sGiven(:assert_content) { -> (c) { assert_file file, c }",
+          "\n\s\sdescribe"
+        ].join("\n")
+      end
+
+      # def describe_block_for(dummy)
+      #   dummy_path = dummy.split('/dummy/').last
+      #   [
+      #     "\n\s\sdescribe '#{dummy_path}' do",
+      #     "\s\s\s\sGiven { rollon }",
+      #     "\s\s\s\sGiven(:file) { '#{dummy_path}' }",
+      #     "\s\sGiven { rollon(__dir__) }",
+      #     "\n\s\send"
+      #   ].join("\n")
+      # end
+
+
+      def dummies_path_for(test_file)
+        path = test_file.split('/_test.rb').first
+        test_file
+        Dir.glob("#{path}/dummy/**/*")
+      end
+
+
+      def dummies_path_for(test_file)
+        path = test_file.split('/_test.rb').first
+        test_file
+        Dir.glob("#{path}/dummy/**/*")
+      end
+
+      def dummy_assertions(dummy_file_name)
+        [
+          "Given(:file) { '#{dummy_file_name} ' } ",
+          "Then { assert_file '#{dummy_file_name}" ,
+          "And  { assert_file '#{dummy_file_name}, 'some string",
+          "And  { assert_content 'some string' }",
+        ]
+      end
+
+      def indent(array)
+        array.map { |s| "\s\s#{s}"}
+      end
+
+      def describe_dummy_file(dummy_file_name)
+        <<-HEREDOC
+
+  describe 'must generate' do
+    describe '#{dummy_file_name}' do
+      Given(:file) { '#{dummy_file_name}' }
+      Then { assert_file file }
+      And  { assert_file file, 'foo' }
+  
+      describe 'must have contents like' do 
+        Given(:assert_content) { -> (c) { assert_file file, c } }
+        
+        Then { assert_content['foo'] }
+      end
+    end 
+  end
+        HEREDOC
+      end
+      # def describe_dummy_file(dummy_file_name)
+      #   array = [
+      #     "describe 'must generate' do"
+      #   ]
+      #   array += indent( dummy_assertions( dummy_file_name ) )
+      #   indent(array).join("\n")
+      # end
+
+
+      def dummy_assertions(dummy_file_name)
+        array = [
+          "describe '#{dummy_file_name}' do",
+          "  Given(:file) { '#{dummy_file_name}' } ",
+          ''
+        ]
+        array += indent(dummy_assert_file(dummy_file_name))
+        array + indent(dummy_assert_content(dummy_file_name))
+      end
+
+      def dummy_assert_content(dummy_file_name)
+        array = [
+          "describe 'content must equal' do ",
+          "  Then { assert_file file }",
+          'end'
+        ]
+        array
+      end
+
+      def dummy_assert_file(dummy_file_name)
+        array = [
+          "describe 'must exist' do ",
+          "  Then { assert_file file }",
+          'end'
+        ]
+        array
+      end
+
+      def dummy_assert_contents(dummy_file_name)
+        [
+          "Then { assert_file '#{dummy_file_name}' }",
+        ].map {|s| "\n#{s}"}
+      end
+
+      def dummy_assert_file_contents(dummy_file_name)
+        [
+          "describe 'file' do",
+          "  Then { assert_file '#{dummy_file_name}' }",
+        ]
+      end
+
+      def dummies_for(adventure_test)
+        Dir.glob("#{dummy_path_for(adventure_test)}/**/*")
+           .map {|f| f.split("#{Dir.pwd}/").last }
+      end
+
+      def dummy_path_for(adventure_test)
+        File.expand_path('../dummy', adventure_test)
+      end
+
+      def story_path_for(adventure_test)
+        File.expand_path('../../..', adventure_test)
       end
 
       def adventure_test_files
--- test/roro/cli/generate/generate_annotations_test.rb
+++ test/roro/cli/generate/generate_annotations_test.rb
@@ -3,17 +3,112 @@
 require 'test_helper'
 
 describe 'Roro::CLI#generate_annotations' do
-  Given(:subject)        { Roro::CLI.new }
-  Given(:workbench)      { 'test_annotate/lib' }
-  Given(:base)           { 'lib/roro/stacks/unstoppable_developer_styles' }
-  Given(:space)          { "#{base}/okonomi/languages/ruby/frameworks/rails" }
-  Given(:file)           { "#{space}/versions/v6_1/test/0/_test.rb"}
-
+  Given(:subject)     { Roro::CLI.new }
+  Given(:workbench)   { 'test_annotate/lib' }
+  Given(:base)        { 'lib/roro/stacks/unstoppable_developer_styles' }
+  Given(:space)       { "#{base}/okonomi/languages/ruby/frameworks/rails" }
+  Given(:file)        { "#{space}/versions/v6_1/test/0/_test.rb" }
+  Given(:story_path)        { "#{space}/versions/v6_1" }
+  Given(:description) { 'adventure::rails-v6_1::0 sqlite & ruby-v2_7' }
   Given(:expected) { /describe 'adventure::rails-v6_1::0 sqlite & ruby-v2_7'/ }
+  Given(:adventure_test) { file }
+  Given(:dummy) { "#{story_path}/test/0/dummy/idiot.yaml" }
 
   Given { subject.generate_annotations }
 
   describe 'after generate' do
     Then  { assert_file file, expected }
+  end
+
+  # def adventure_test_files
+  #   Dir.glob("#{Dir.pwd}/lib/roro/stacks/**/*_test.rb")
+  #      .map {|f| f.split("#{Dir.pwd}/").last }
+  # end
+
+
+  describe '#describe_block_for(dummy)' do
+    Given(:dummies) { subject.dummies_for(file) }
+    Given(:dummy) { dummies.first }
+    # Then { assert_equal 'blah', subject.describe_block_for(dummy) }
+  end
+
+  describe 'description blocks' do
+    describe 'outmost' do
+      Then { assert_match description, subject.describe_outmost(description) }
+
+      describe '#describe_outer_before_actions' do
+        # Then { assert_match description, subject.describe_before_actions(file) }
+
+        describe 'inner' do
+          # Then { assert_match description, subject.describe_outmost(description) }
+        end
+      end
+    end
+
+    describe 'after_actions' do
+      # Then { assert_match description, subject.describe_after_actions(file) }
+    end
+
+    describe '#dummy_fille_with' do
+      Given(:dummy) { "#{space}/versions/v6_1/test/0/dummy/idiot.yml" }
+      # Then { assert_match 'blah', subject.describe_dummy_file_with(dummy) }
+    end
+
+    describe '#description_helper(adventure_test)' do
+      # Given(:result) { subject.description_helper('adventure_test') }
+
+      # Then { assert_match "describe 'idiot.yml'", result }
+      # Then { save_output(result) }
+    end
+    describe '#describe_dummy_file' do
+      Given(:result) { subject.describe_dummy_file('idiot.yml') }
+
+      # Then { assert_match "describe 'idiot.yml'", result }
+      # Then { save_output(result) }
+    end
+
+    describe '#dummy_assertions' do
+      Given(:result) { subject.dummy_assertions('idiot.yml') }
+
+      Then { assert_includes result, "  Given(:file) { 'idiot.yml' }" }
+      # And { save_output(result) }
+    end
+
+    describe '#dummy_assert_file' do
+      Given(:result) { subject.dummy_assert_file('idiot.yml') }
+      Then { assert_includes result, "  Then { assert_file 'idiot.yml' }" }
+    end
+
+    def save_output(result)
+      File.open("#{@roro_dir}/tmp/blah.rb", "w") do |f|
+        f.write(result)
+      end
+    end
+
+    describe '#dummy_assert_contents' do
+      Given(:result) { subject.dummy_assert_file('idiot.yml') }
+      Then { assert_includes result, "  Then { assert_file 'idiot.yml' }" }
+    end
+
+    describe '#dummies_for(adventure_test)' do
+      Given(:result) { subject.dummies_for(adventure_test) }
+      Then { assert_includes result, dummy }
+    end
+
+    describe '#dummy_path_for(adventure_test)' do
+      Given(:result) { subject.dummy_path_for(adventure_test) }
+      Then { assert_match 'test/0/dummy', result }
+      And  { refute_match 'test/dummy/', result }
+    end
+
+    describe '#story_path_for(adventure_test)' do
+      Given(:result) { subject.story_path_for(adventure_test) }
+      Then { assert_match "versions/v6_1", result }
+      And  { refute_match "versions/v6_1/", result }
+    end
+
+    describe '#adventure_test_files' do
+      Then { assert_includes subject.adventure_test_files, adventure_test }
+    end
   end
 end
