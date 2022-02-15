f2027cc4712ce6dc79ac0385ebfdb166796cf5cb
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-02-14 20:39:36 -0800

    almost ready too push

--- mise/scripts/rollon-pull.sh
+++ mise/scripts/rollon-pull.sh
@@ -11,3 +11,27 @@ sudo docker run \
 
 sudo docker run  -it handsomefencer/roro:latest roro rollon
 
+
+#!/bin/bash
+
+#cd $DST
+#
+#sudo docker run \
+#  --name artifact \
+#  -v /var/run/docker.sock:/var/run/docker.sock \
+#  -u 0 \
+#  -it handsomefencer/roro:latest roro rollon
+#
+#docker cp artifact:/. .
+#
+##  -i roar:latest printf "1\n3\n1\n2\n2\n1\na\n" | roro rollon
+#
+##1
+##
+##
+##1
+##3
+##1
+##2
+##2
+##1
