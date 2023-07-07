97200c8b6203e8ed296b668488a9c4895b7970c7
schadenfred <fred.schoeneman@gmail.com>
2023-07-06 23:48:31 -0700

    Adds redis

--- test/roro/stacks/okonomi/mariadb/6_1/_test.rb
+++ test/roro/stacks/okonomi/mariadb/6_1/_test.rb
@@ -6,6 +6,7 @@ describe '1 -> 1 -> 1: database: mariadb, rails version: 6.1' do
   Given(:workbench) {}
 
   Given do
+    debuggerer
     rollon(__dir__)
   end
 
--- test/roro/stacks/okonomi/shared_tests.rb
+++ test/roro/stacks/okonomi/shared_tests.rb
@@ -3,5 +3,24 @@
 require_relative '../shared_tests'
 
 def assert_stacked_okonomi
+  assert_stacked_redis
+  assert_stacked_sidekiq
 end
 
+def assert_stacked_sidekiq
+  f = 'docker-compose.yml'
+  assert_yaml(f, :services, :sidekiq, :image, 'unstoppable')
+end
+
+def assert_stacked_redis
+  f = 'mise/env/base.env'
+  assert_file(f, %r{REDIS_URL=redis://redis:6379/0})
+  assert_yaml('docker-compose.yml')
+
+  f = 'docker-compose.yml'
+  assert_file(f, /\nvolumes:\n\s\sdb_data/)
+  assert_file(f, /\s\sgem_cache/)
+  assert_file(f, /\s\snode_modules/)
+  assert_file(f, /\s\sredis/)
+  assert_yaml(f, :services, :redis, :image, 'redis:7.0-alpine')
+end
