dcbccd5d88f3291778789b1fccb02598c9e13db6
schadenfred <fred.schoeneman@gmail.com>
2023-07-15 17:21:37 -0700

    Generate tests

--- test/roro/stacks/okonomi/mariadb/7_0/importmap/none/_test.rb
+++ test/roro/stacks/okonomi/mariadb/7_0/importmap/none/_test.rb
@@ -2,10 +2,19 @@
 
 require_relative '../shared_tests'
 
-describe '1 okonomi -> 1 mariadb -> 2 7_0 -> 1 importmap -> 1 none' do
+describe '1 -> 1 -> 2: database: mariadb, rails version: 7.0' do
   Given(:workbench) {}
-  
-  Given do 
-    rollon(__dir__) 
-  end 
+
+  Given do
+    rollon(__dir__)
+  end
+
+  Then do
+    assert_stacked_stacks
+    assert_stacked_okonomi
+    assert_stacked_mariadb
+    assert_stacked_7_0
+    refute_stacked_compose_service_redis
+    refute_stacked_compose_service_sidekiq
+  end
 end
--- test/roro/stacks/okonomi/mariadb/7_0/vite/sidekiq/_test.rb
+++ test/roro/stacks/okonomi/mariadb/7_0/vite/sidekiq/_test.rb
@@ -2,10 +2,29 @@
 
 require_relative '../shared_tests'
 
-describe '1 okonomi -> 1 mariadb -> 2 7_0 -> 2 vite -> 2 sidekiq' do
+describe '1 -> 1 -> 2: database: mariadb, rails version: 7.0' do
   Given(:workbench) {}
-  
-  Given do 
-    rollon(__dir__) 
-  end 
+
+  Given do
+    debuggerer
+    rollon(__dir__)
+  end
+  focus
+  Then do
+    assert_stacked_stacks
+    assert_stacked_okonomi
+    assert_stacked_mariadb
+    assert_stacked_7_0
+    assert_stacked_compose_service_redis
+    assert_stacked_compose_service_sidekiq
+    assert_stacked_compose_app_depends_on
+    assert_stacked_compose_service_vite
+  end
+
+  def assert_stacked_compose_service_vite
+    assert_file('Gemfile', /gem ["']vite_rails["']/)
+
+    a = ['docker-compose.yml', :services]
+    # assert_yaml(*a, :db, :image, 'mariassdb:11.0')
+  end
 end
