fde64229c05a7aa5b2fc1c68bb7fa3a261feebc1
schadenfred <fred.schoeneman@gmail.com>
2023-07-19 17:09:56 -0700

    Generate flutter adventure and tests

---
+++ test/roro/stacks/flutter/_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../shared_tests'
+
+describe '1 flutter' do
+  Given(:workbench) {}
+  
+  Given do 
+    rollon(__dir__) 
+  end 
+end
---
+++ test/roro/stacks/ruby/omakase/_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../shared_tests'
+
+describe '2 ruby -> 2 omakase' do
+  Given(:workbench) {}
+  
+  Given do 
+    rollon(__dir__) 
+  end 
+end
