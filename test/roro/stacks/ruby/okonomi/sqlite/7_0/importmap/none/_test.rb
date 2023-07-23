716e97941344eeae33c5d74aaecf187d8ebe8ccf
schadenfred <fred.schoeneman@gmail.com>
2023-07-23 14:49:13 -0700

    Speedking up buidl layers for rails apps

--- test/roro/stacks/ruby/okonomi/sqlite/7_0/importmap/none/_test.rb
+++ test/roro/stacks/ruby/okonomi/sqlite/7_0/importmap/none/_test.rb
@@ -6,8 +6,9 @@ describe '1 okonomi -> 4 sqlite -> 2 7_0 -> 1 none' do
   Given(:workbench) {}
 
   Given do
+    debuggerer
     rollon(__dir__)
   end
-
+  focus
   Then { assert_correct_manifest(__dir__) }
 end
