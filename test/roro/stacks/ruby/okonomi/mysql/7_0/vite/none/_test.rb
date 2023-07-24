d9390ba6544fe561934cf0a67a29468cfbd81165
schadenfred <fred.schoeneman@gmail.com>
2023-07-23 17:39:00 -0700

    Remove shared_tests

--- test/roro/stacks/ruby/okonomi/mysql/7_0/vite/none/_test.rb
+++ test/roro/stacks/ruby/okonomi/mysql/7_0/vite/none/_test.rb
@@ -6,9 +6,8 @@ describe '1 -> 2 -> 2: ' do
   Given(:workbench) {}
 
   Given do
-    debuggerer
     rollon(__dir__)
   end
-  focus
+
   Then { assert_correct_manifest(__dir__) }
 end
--- test/roro/stacks/ruby/okonomi/mysql/7_0/vite/shared_tests.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../shared_tests'
-
-def assert_stacked_vite
-end
-
