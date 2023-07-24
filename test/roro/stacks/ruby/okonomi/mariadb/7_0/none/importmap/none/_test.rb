28a619e289e60ac75e3608f0a939415a9b959060
schadenfred <fred.schoeneman@gmail.com>
2023-07-23 20:15:52 -0700

    Guess about directories

---
+++ test/roro/stacks/ruby/okonomi/mariadb/7_0/none/importmap/none/_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe '1 -> 1 -> 2: database: mariadb, rails version: 7.0' do
+  Given(:workbench) {}
+
+  Given do
+    rollon(__dir__)
+  end
+
+  Then { assert_correct_manifest(__dir__) }
+end
