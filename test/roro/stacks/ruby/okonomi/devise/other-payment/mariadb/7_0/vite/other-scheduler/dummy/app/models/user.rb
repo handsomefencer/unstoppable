f58563ede56025f203935fc4a3e7ca810c47059f
Fred Schoeneman <fred.schoeneman@gmail.com>
2023-07-25 17:52:20 -0700

    ignored

--- test/roro/stacks/ruby/okonomi/devise/other-payment/mariadb/7_0/vite/other-scheduler/dummy/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise :confirmable, :trackable, modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :confirmable, :trackable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
