4425aa5dfb51f798732723dcae53736735f33117
schadenfred <fred.schoeneman@gmail.com>
2023-08-04 23:05:46 -0700

    Trying known setup from teren

--- mise/scripts/rebuild.sh
+++ mise/scripts/rebuild.sh
@@ -7,7 +7,7 @@ roro=~/work/handsomefencer/gems/roro
 
 cd ${sandbox_dir} 
 docker-compose down
-
+docker volume rm $(docker volume ls -q)
 cd ${roro}
 
 sudo rm -rf ${sandbox_dir}
@@ -20,6 +20,7 @@ dc build roro
 
 mkdir -p ${sandbox_dir}
 cd ${sandbox_dir} 
+docker volume rm $(docker volume ls --filter name=$app -q)
 
 docker rm artifact
 docker run \
@@ -27,9 +28,9 @@ docker run \
   -v $PWD:/artifact \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -u 0 \
-  -it handsomefencer/roro sh -c "printf '2\n1\n1\n2\n3\n2\n2\n2\na\n' | roro rollon"
+  -it handsomefencer/roro sh -c "printf '2\n1\n2\n3\n2\n2\n1\na\n' | roro rollon"
   # -it handsomefencer/roro roro rollon
-
+# 2\n1\n2\n3\n2\n2\n2\n1\na\n
 schown  
 
 dc build app 
