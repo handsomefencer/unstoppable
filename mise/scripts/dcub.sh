5dce450f67caaa5811b3a0fd7d32dcf73939a75a
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-02-25 22:24:30 -0800

    Remove all okonomi sqlite stories.

---
+++ mise/scripts/dcub.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -a 
+
+docker compose down
+docker system prune -f
+docker image rm -f $(docker images unstoppable)
+docker image rm -f $(docker images handsomefencer/roro)
+docker image rm -f $(docker images handsomefencer/roro-builder)
+docker image rm -f $(docker images handsomefencer/ruby)
+docker volume rm -f $(docker volume ls --filter name=artifact*)
+docker system prune -f
+docker volume 
+docker volume rm $(docker volume ls --filter name=gem_cache*)
+
+docker images
+dcd 
+
+dc build --no-cache ruby
+dc build --no-cache builder
+dc build --no-cache developer
+dc build --no-cache guard
+dc run --rm guard
--- test/roro/stacks/ruby/omakase/_test.rb
+++ test/roro/stacks/ruby/omakase/_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 describe '2 ruby -> 2 omakase' do
   Given(:workbench) {}
-  
+
   Given do
     rollon(__dir__)
   end
