6abd52f97ef936788d81b46201450bd831ee1957
schadenfred <fred.schoeneman@gmail.com>
2024-02-26 18:30:28 -0800

    Improve build speed with caching

--- Guardfile
+++ Guardfile
@@ -9,11 +9,11 @@ Pry.config.output = STDOUT
 minitest_options = {
   test_folders: ['test'],
   all_after_pass: false,
-  all_on_start: false,
+  all_on_start: true,
   all_env: {
     'DEBUGGERER' =>
-      'false'
-      # 'true'
+      # 'false'
+      'true'
   }
 }
 
--- mise/scripts/dcub.sh
+++ mise/scripts/dcub.sh
@@ -12,12 +12,13 @@ docker volume rm -f $(docker volume ls --filter name=artifact*)
 docker system prune -f
 docker volume 
 docker volume rm $(docker volume ls --filter name=gem_cache*)
-
+docker system prune -af --volumes
 docker images
 dcd 
 
-dc build --no-cache ruby
-dc build --no-cache builder
-dc build --no-cache developer
-dc build --no-cache guard
+dc build --no-cache --with-dependencies guard
+# dc build --no-cache ruby
+# dc build --no-cache builder
+# dc build --no-cache developer
+# dc build --no-cache guard
 dc run --rm guard
