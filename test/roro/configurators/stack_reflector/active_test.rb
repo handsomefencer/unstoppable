b3c20bee0e16de2e5ca494df45c2155a3bc5166e
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-03-12 21:53:06 -0700

    Move dc up a level

--- test/roro/configurators/stack_reflector/active_test.rb
+++ test/roro/configurators/stack_reflector/active_test.rb
@@ -8,10 +8,10 @@ describe Roro::Configurators::StackReflector do
 
   describe '[:pretty_tags]' do
     Given(:picks) { %i[1 1 1 1 3 1 1 1] }
-    Given(:expected) {
-      %w[ Ruby_on_Rails Alpine_Linux Docker_Compose
-      Git Devise Stripe Mariadb Vite Redis Sidekiq ]
-    }
+    Given(:expected) { %w[
+      Docker_Compose Ruby_on_Rails Alpine_Linux
+      Git Devise Stripe Mariadb Vite Redis Sidekiq
+    ] }
     Then { assert_equal(expected, adventure.dig(:pretty_tags))}
     And { assert_equal('7.1', adventure.dig(:versions, 'rails'))}
   end
