6d095ef2d76af8cfb2ccd7c4e47f667ecb6cfbbf
schadenfred <fred.schoeneman@gmail.com>
2024-05-10 19:11:07 -0700

    wip

--- test/roro/stacks/sqlite/esbuild/omakase/_test.rb
+++ test/roro/stacks/sqlite/esbuild/omakase/_test.rb
@@ -4,10 +4,10 @@ require 'test_helper'
 
 describe '4 SQLite -> 1 ESBuild -> 2 omakase' do
   Given(:workbench) {}
-  
+
   Given do
+    debuggerer
     rollon(__dir__)
   end
-
   Then { assert_correct_manifest(__dir__) }
 end
--- test/roro/stacks/sqlite/importmaps/omakase/_test.rb
+++ test/roro/stacks/sqlite/importmaps/omakase/_test.rb
@@ -9,11 +9,11 @@ describe '4 SQLite -> 3 Importmaps -> 2 omakase' do
     debuggerer
     rollon(__dir__)
   end
-  focus
+focus
   Then { assert_correct_manifest(__dir__) }
 
   describe 'will not have db container' do
-    Then  do
+    Then do
       assert 'mise/containers/app'
       refute_file 'mise/containers/db'
     end
@@ -26,10 +26,13 @@ describe '4 SQLite -> 3 Importmaps -> 2 omakase' do
     end
   end
 
-  describe 'will not have db service' do
+  describe 'will not have dockerfile node cache' do
+    Given(:file) { 'mise/containers/app/Dockerfile.builder.cache' }
+    Given(:content) { 'mise/containers/app/Dockerfile.builder.cache' }
+
     Then do
-      assert_file('docker-compose.vendor.yml', /roro:/)
-      assert_file('Dockerfile.builder.cache')
+      assert_file(file, /RUN bundle update --bundler/)
+      refute_content(file, /COPY package.json yarn.lock/)
     end
   end
 end
