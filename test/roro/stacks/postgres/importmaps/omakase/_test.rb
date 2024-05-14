d06a052cf7efc55038caa4aa60298c2d868508cf
schadenfred <fred.schoeneman@gmail.com>
2024-05-14 12:57:23 -0700

    SQLite with Importmaps

--- test/roro/stacks/postgres/importmaps/omakase/_test.rb
+++ test/roro/stacks/postgres/importmaps/omakase/_test.rb
@@ -6,7 +6,21 @@ describe '3 Postgres -> 3 importmap -> 2 omakase' do
   Given(:workbench) {}
 
   Given do
+    debuggerer
     rollon(__dir__)
   end
+
+focus
   Then { assert_correct_manifest(__dir__) }
+
+  describe 'will not have dockerfile node cache' do
+    Given(:file) { 'mise/containers/app/Dockerfile.builder.cache' }
+    Given(:content) { 'mise/containers/app/Dockerfile.builder.cache' }
+
+    Then do
+      assert_file(file, /RUN bundle update --bundler/)
+      refute_content(file, /COPY package.json yarn.lock/)
+    end
+  end
+
 end
