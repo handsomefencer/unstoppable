08f586064da04788f2af5466f020fbc9e2b3823b
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-05-16 18:54:01 -0700

    All 4 js approaches working

--- test/roro/stacks/sqlite/bun/omakase/_test.rb
+++ test/roro/stacks/sqlite/bun/omakase/_test.rb
@@ -6,9 +6,29 @@ describe '4 SQLite -> 1 Bun -> 2 omakase' do
   Given(:workbench) {}
 
   Given do
-    debuggerer
     rollon(__dir__)
   end
-focus
   Then { assert_correct_manifest(__dir__) }
+
+  describe 'dockerfiles' do
+    describe 'builder.cache' do
+      Then do
+        refute_content('mise/containers/app/Dockerfile.builder.cache', /yarn/)
+      end
+    end
+
+    describe 'builder.deps.dev' do
+      Given(:file) { 'mise/containers/app/Dockerfile.builder.deps.dev' }
+      Then do
+        refute_content(file, /node/)
+      end
+    end
+
+    describe 'builder.deps.dev' do
+      Given(:file) { 'mise/containers/app/Dockerfile.development' }
+      Then do
+        refute_content(file, /COPY package.json/)
+      end
+    end
+  end
 end
