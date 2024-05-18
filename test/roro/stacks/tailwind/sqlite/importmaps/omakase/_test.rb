167ce931e4676f688b7aa08bcad97503db1908f2
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-05-17 23:18:19 -0700

    Add default css processor

--- test/roro/stacks/tailwind/sqlite/importmaps/omakase/_test.rb
+++ test/roro/stacks/tailwind/sqlite/importmaps/omakase/_test.rb
@@ -6,7 +6,10 @@ describe '5 tailwind -> 4 SQLite -> 3 Importmaps -> 2 omakase' do
   Given(:workbench) {}
 
   Given do
+    debuggerer
     rollon(__dir__)
   end
+
+  focus
   Then { assert_correct_manifest(__dir__) }
 end
