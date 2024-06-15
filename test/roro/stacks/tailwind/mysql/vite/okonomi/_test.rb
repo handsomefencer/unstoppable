92c73ed01e643ff37a5b5e51828e46f7305e3f18
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-06-15 13:54:12 -0700

    Add db_pkg value

--- test/roro/stacks/tailwind/mysql/vite/okonomi/_test.rb
+++ test/roro/stacks/tailwind/mysql/vite/okonomi/_test.rb
@@ -4,10 +4,10 @@ require 'stack_test_helper'
 
 describe '6 tailwind -> 2 MySQL -> 4 Vite -> 1 okonomi' do
   Given(:workbench) {}
-  
-  Given do
-    # debugger
-  end
 
+  Given do
+    debuggerer
+  end
+focus
   Then { assert_correct_manifest(__dir__) }
 end
