88abe4801c32d6e83927e227f8e4d14b40f152cc
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-06-29 22:41:53 -0700

    Test build

--- mise/scripts/build.circle.sh
+++ mise/scripts/build.circle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose run --rm roro roro generate:obfuscated
+# docker compose run --rm roro roro generate:obfuscated
 git add .
 git commit -m 'Test build'
 git push origin 2408
