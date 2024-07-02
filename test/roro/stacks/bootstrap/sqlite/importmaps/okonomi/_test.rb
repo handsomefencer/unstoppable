84a5a38a52c6cc7707316695926f20514eb313cf
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-07-02 14:52:22 -0700

    Refactor ci to build test image in build job

--- test/roro/stacks/bootstrap/sqlite/importmaps/okonomi/_test.rb
+++ test/roro/stacks/bootstrap/sqlite/importmaps/okonomi/_test.rb
@@ -4,11 +4,11 @@ require 'stack_test_helper'
 
 describe '1 Bootstrap -> 4 SQLite -> 3 Importmaps -> 1 okonomi' do
   Given(:workbench) {}
-  
+
   Given do
     #skip
     debuggerer
   end
-#focus
+focus
   Then { assert_correct_manifest(__dir__) }
 end
