e22cba0e320700e973e7c51dd597595dcca836f6
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-07-01 22:56:07 -0700

    Test build

--- mise/scripts/build.test.sh
+++ mise/scripts/build.test.sh
@@ -4,4 +4,8 @@ docker compose down
 
 . mise/scripts/docker.prune.sh
 
-docker compose build --with-dependencies development
+# docker compose build --with-dependencies roro-development
+docker compose build --with-dependencies test
+docker compose run --rm test bundle 
+docker compose build test
+docker compose run --rm test bundle exec rake test:roro
