297db0011be831c3cf135e34b2c23deca2561d35
schadenfred <fred.schoeneman@gmail.com>
2025-10-29 22:43:53 -0700

    add 7.2 option, reorganizes tests

---
+++ test/roro/stacks/skip_css/sqlite/importmaps/7_2/_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'stack_test_helper'
+
+describe '5 skip_css -> 4 SQLite -> 3 Importmaps -> 2 7_2' do
+  Given(:workbench) {}
+  
+  Given do
+    # skip
+    debuggerer
+  end
+  # focus
+  Then { assert_correct_manifest(__dir__) }
+end
