101397624ce81a7c587a93e0d3c275880db9df72
schadenfred <fred.schoeneman@gmail.com>
2025-10-31 16:16:52 -0700

    initial commit

--- mise/scripts/reference.rebuild.sh
+++ mise/scripts/reference.rebuild.sh
@@ -5,7 +5,9 @@ export sandbox_dir=~/Work/sandbox/${app}
 export roro=~/Work/handsomefencer/unstoppable
 
 # . mise/scripts/docker.prune.sh
+. mise/scripts/reference.prune.sh
 . mise/scripts/reference.remove.sh
+. mise/scripts/docker.info.sh
 . mise/scripts/build.production.sh
 . mise/scripts/reference.build.sh
 . mise/scripts/reference.up.sh
