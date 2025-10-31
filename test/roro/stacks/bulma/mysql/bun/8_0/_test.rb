1ee598c85ab9b3ca9aae0cfda337ae40d3f06659
schadenfred <fred.schoeneman@gmail.com>
2025-10-30 19:39:31 -0700

    initial commit

---
+++ test/roro/stacks/bulma/mysql/bun/8_0/_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'stack_test_helper'
+
+describe '2 Bulma -> 2 MySQL -> 1 Bun -> 3 8_0' do
+  Given(:workbench) {}
+  
+  Given do
+    # skip
+    # debuggerer
+  end
+  # focus
+  Then { assert_correct_manifest(__dir__) }
+end
