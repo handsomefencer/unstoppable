1c9ba5db8fc91775ef0cb9154a53553ab79b2c04
schadenfred <fred.schoeneman@gmail.com>
2025-10-30 21:38:17 -0700

    Generate 8.1 tests

---
+++ test/roro/stacks/skip_css/sqlite/esbuild/8_1/_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'stack_test_helper'
+
+describe '5 skip_css -> 4 SQLite -> 2 ESBuild -> 4 8_1' do
+  Given(:workbench) {}
+  
+  Given do
+    # skip
+    # debuggerer
+  end
+  # focus
+  Then { assert_correct_manifest(__dir__) }
+end
