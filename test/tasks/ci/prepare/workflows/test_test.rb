52b3cad7278aecaac9023d7c0cdc8cd3cf854097
schadenfred <fred.schoeneman@gmail.com>
2025-11-05 14:02:14 -0800

    Remove focus statement

--- test/tasks/ci/prepare/workflows/test_test.rb
+++ test/tasks/ci/prepare/workflows/test_test.rb
@@ -21,7 +21,6 @@ describe 'rake ci:prepare:workflows:test' do
   describe 'with one argument' do
     Given { args << 'sqlite importmaps okonomi ; tailwind bun omakase' }
     Given { execute }
-    focus
     Then do
       assert_equal 1, globdir(stacks).size
       assert_equal 1, globdir(roro).size
